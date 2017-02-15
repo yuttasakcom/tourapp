e22322ae1de0ddaadf257915c484fdda90d14399
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 22:10:32 +0700

    signup can create new company

--- controllers/company_auth_controller.js
+++ controllers/company_auth_controller.js
@@ -1,5 +1,11 @@
+const Company = require('../models/company')
+
 module.exports = {
-  signup(req, res) {
-    res.send({ msg: 'signup' })
+  signup(req, res, next) {
+  	const companyProps = req.body
+
+  	Company.create(companyProps)
+  		.then(company => res.status(201).send(company))
+  		.catch(next)
   }
 }
