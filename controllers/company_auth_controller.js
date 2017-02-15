0019a1f8d6232369897807f6cac47d37365c1882
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 22:55:31 +0700

    signup can not be use a dupplicate email

--- controllers/company_auth_controller.js
+++ controllers/company_auth_controller.js
@@ -2,10 +2,16 @@ const Company = require('../models/company')
 
 module.exports = {
   signup(req, res, next) {
-  	const companyProps = req.body
-
-  	Company.create(companyProps)
-  		.then(company => res.status(201).send(company))
-  		.catch(next)
+    const companyProps = req.body
+    Company.findOne({ email: companyProps.email })
+      .then(existingUser => {
+        if (existingUser) {
+          return res.status(422).send({ error: 'Email is in use' })
+        }
+        Company.create(companyProps)
+          .then(company => res.status(201).send(company))
+          .catch(next)
+      })
+      .catch(next)
   }
 }
