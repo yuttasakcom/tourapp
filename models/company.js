a52d704a80db60769a7c465a38169d0b5490f838
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 22:09:53 +0700

    add company schema

---
+++ models/company.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const companySchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true
+	},
+	password: String
+})
+
+const Company = mongoose.model('Company', companySchema)
+
+module.exports = Company
