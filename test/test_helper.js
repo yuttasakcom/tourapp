31758f84cb9a6dad4d8a75898e496f923b9fcdc1
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 21:44:42 +0700

    fix done bug in test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 before(done => {
 	mongoose.connect('mongodb://localhost/tourapp_test')
 	mongoose.connection
-		.once('open', done)
+		.once('open', () => done())
 		.on('error', err => console.warn('Warning', err))
 })
 
 beforeEach(done => {
 	const { companies } = mongoose.connection.collections
 	companies.drop()
-		.then(done)
-		.catch(done)
+		.then(() => done())
+		.catch(() => done())
 })
