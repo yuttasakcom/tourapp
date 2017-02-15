5f62518816bb54e1b248555b06a80b9c516a7580
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 22:54:56 +0700

    signup can not be use a duplicate email

--- test/controllers/company_auth_controller_test.js
+++ test/controllers/company_auth_controller_test.js
@@ -6,7 +6,7 @@ const Company = mongoose.model('Company')
 
 describe('company authentication', () => {
 
-  it('POST /companies/signup create a new company', done => {
+  it('POST to /companies/signup create a new company', done => {
     Company.count().then(count => {
       request(app)
         .post('/companies/signup')
@@ -22,6 +22,27 @@ describe('company authentication', () => {
             expect(count + 1).to.equal(newCount)
             done()
           })
+        })
+    })
+  })
+
+  it('signup can not be use a duplicate email', done => {
+    const companyProps = {
+      email: 'company1@test.com',
+      password: '1234'
+    }
+    const company = new Company(companyProps)
+
+    company.save().then(() => {
+      request(app)
+        .post('/companies/signup')
+        .send(companyProps)
+        .expect(422)
+        .end((err, res) => {
+          if (err) return done(err)
+
+          expect(res.body.error).to.equal('Email is in use')
+          done()
         })
     })
   })
