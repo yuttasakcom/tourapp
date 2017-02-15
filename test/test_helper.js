09c0c2616993840df4b5dbc767bf9e70e07dd4db
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 20:51:06 +0700

    setup test_helper

---
+++ test/test_helper.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+before(done => {
+	mongoose.connect('mongodb://localhost/tourapp_test')
+	mongoose.connection
+		.once('open', done)
+		.on('error', err => console.warn('Warning', err))
+})
+
+beforeEach(done => {
+	const { companies } = mongoose.connection.collections
+	companies.drop()
+		.then(done)
+		.catch(done)
+})
