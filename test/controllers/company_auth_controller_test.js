e8fbe971c4c8e272903f7ae7f4451e5e5e31e535
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-15 22:59:20 +0700

    refactor test

--- test/controllers/company_auth_controller_test.js
+++ test/controllers/company_auth_controller_test.js
@@ -6,44 +6,47 @@ const Company = mongoose.model('Company')
 
 describe('company authentication', () => {
 
-  it('POST to /companies/signup create a new company', done => {
-    Company.count().then(count => {
-      request(app)
-        .post('/companies/signup')
-        .send({
-          email: 'company1@test.com',
-          password: '1234'
-        })
-        .expect(201)
-        .end((err, res) => {
-          if (err) return done(err)
+  describe('signup', () => {
 
-          Company.count().then(newCount => {
-            expect(count + 1).to.equal(newCount)
+    it('create a new company', done => {
+      Company.count().then(count => {
+        request(app)
+          .post('/companies/signup')
+          .send({
+            email: 'company1@test.com',
+            password: '1234'
+          })
+          .expect(201)
+          .end((err, res) => {
+            if (err) return done(err)
+
+            Company.count().then(newCount => {
+              expect(count + 1).to.equal(newCount)
+              done()
+            })
+          })
+      })
+    })
+
+    it('can not be use a duplicate email', done => {
+      const companyProps = {
+        email: 'company1@test.com',
+        password: '1234'
+      }
+      const company = new Company(companyProps)
+
+      company.save().then(() => {
+        request(app)
+          .post('/companies/signup')
+          .send(companyProps)
+          .expect(422)
+          .end((err, res) => {
+            if (err) return done(err)
+
+            expect(res.body.error).to.equal('Email is in use')
             done()
           })
-        })
-    })
-  })
-
-  it('signup can not be use a duplicate email', done => {
-    const companyProps = {
-      email: 'company1@test.com',
-      password: '1234'
-    }
-    const company = new Company(companyProps)
-
-    company.save().then(() => {
-      request(app)
-        .post('/companies/signup')
-        .send(companyProps)
-        .expect(422)
-        .end((err, res) => {
-          if (err) return done(err)
-
-          expect(res.body.error).to.equal('Email is in use')
-          done()
-        })
+      })
     })
   })
 })
