32d64ba95bcf707886441d898b224f69e1e0e8dc
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-16 11:24:04 +0700

    refactor check duplicate email in model instead

--- controllers/company_auth_controller.js
+++ controllers/company_auth_controller.js
@@ -3,24 +3,16 @@ const Company = require('../models/company')
 module.exports = {
   signup(req, res, next) {
     const companyProps = req.body
-    Company.findOne({ email: companyProps.email })
-      .then(existingUser => {
-        if (existingUser) {
-          let err = new Error('Email is in use')
-          err.status = 422
-          next(err)
-        }
-        const company = new Company(companyProps)
-        const validationErr = company.validateSync()
-        if (validationErr) {
-          let err = new Error('Must provide email or password')
-          err.status = 422
-          next(err)
-        }
-        company.save()
-          .then(company => res.status(201).send({ token: 'mock' }))
-          .catch(next)
-      })
+    const company = new Company(companyProps)
+    const validationErr = company.validateSync()
+    if (validationErr) {
+      let err = new Error('Must provide email or password')
+      err.status = 422
+      return next(err)
+    }
+
+    company.save()
+      .then(company => res.status(201).send({ token: 'mock' }))
       .catch(next)
   }
 }
--- models/company.js
+++ models/company.js
@@ -2,16 +2,32 @@ const mongoose = require('./mongoose')
 const Schema = mongoose.Schema
 
 const companySchema = new Schema({
-	email: {
-		type: String,
-		unique: true,
-		lowercase: true,
-		required: [true, 'Name is required']
-	},
-	password: {
-		type: String,
-		required: [true, 'Password is required']
-	}
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, 'Name is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  }
+})
+
+companySchema.pre('save', function(next) {
+  const Company = mongoose.model('Company')
+  Company.findOne({ email: this.email })
+    .then(existingCompany => {
+      if (existingCompany) {
+        this.invalidate('email', 'Email is in use')
+        let err = new Error('Email is in use')
+        err.status = 422
+        next(err)
+      } else {
+      	next()
+      }
+    })
+    .catch(next)
 })
 
 const Company = mongoose.model('Company', companySchema)
