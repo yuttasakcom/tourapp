72be134a4f8af5309173fc1b7373c35c7b1740b5
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-16 22:30:05 +0700

    add require companny auth middleware to /profile route

--- routes/company_route.js
+++ routes/company_route.js
@@ -5,10 +5,11 @@ const {
 } = require('../controllers/company_controller')
 const companyPassport = require('../services/company_passport')
 const requireCompanySignin = companyPassport.authenticate('local', { session: false })
+const requireCompanyAuth = companyPassport.authenticate('jwt', { session: false })
 const router = require('express').Router()
 
 router.post('/signup', signup)
 router.post('/signin', requireCompanySignin, signin)
-router.get('/profile', profile)
+router.get('/profile', requireCompanyAuth, profile)
 
 module.exports = router
