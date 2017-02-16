1815ed9cef94bc372c6d78255a01d201962b83ca
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-16 21:16:19 +0700

    add requireCompanySignin middleware to /signin route

--- routes/company_auth_route.js
+++ routes/company_auth_route.js
@@ -2,9 +2,11 @@ const {
   signup,
   signin
 } = require('../controllers/company_auth_controller')
+const companyPassport = require('../services/company_passport')
+const requireCompanySignin = companyPassport.authenticate('local', { session: false })
 const router = require('express').Router()
 
 router.post('/signup', signup)
-router.post('/signin', signin)
+router.post('/signin', requireCompanySignin, signin)
 
 module.exports = router
