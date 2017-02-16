506da6091f34b8c53023983bf87315874366dde0
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-16 10:52:27 +0700

    remove err stack log and can add custom err status

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use((req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send({ error: err.message })
+  res.status(err.status || 500).send({ error: err.message })
 })
 
 module.exports = app
