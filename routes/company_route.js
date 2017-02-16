0d5f86470125d9c4813b5c6968b73a843feacb5d
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-16 22:16:31 +0700

    add /companies/profile route

--- routes/company_route.js
+++ routes/company_route.js
@@ -1,6 +1,7 @@
 const {
   signup,
-  signin
+  signin,
+  profile
 } = require('../controllers/company_controller')
 const companyPassport = require('../services/company_passport')
 const requireCompanySignin = companyPassport.authenticate('local', { session: false })
@@ -8,5 +9,6 @@ const router = require('express').Router()
 
 router.post('/signup', signup)
 router.post('/signin', requireCompanySignin, signin)
+router.get('/profile', profile)
 
 module.exports = router
