23c33d189c8a68ab4b12bdc483c9d19b61273e79
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-17 15:34:11 +0700

    add onlyCompany middleware

--- routes/company_route.js
+++ routes/company_route.js
@@ -8,8 +8,18 @@ const requireSignin = passport.authenticate('local', { session: false })
 const requireAuth = passport.authenticate('jwt', { session: false })
 const router = require('express').Router()
 
+const onlyCompany = (req, res, next) => {
+  const role = req.user.role
+  if (role === 'company') {
+    return next()
+  }
+  let err = new Error('Unauthorized')
+  err.status = 401
+  return next(err)
+}
+
 router.post('/signup', signup)
 router.post('/signin', requireSignin, signin)
-router.get('/profile', requireAuth, profile)
+router.get('/profile', requireAuth, onlyCompany, profile)
 
 module.exports = router
