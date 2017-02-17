bf5d28187b22b934277457c7bb22c9625fdf549b
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-17 15:45:55 +0700

    refactoring hasRole middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,5 +6,17 @@ module.exports = {
   handleAnotherError(err, req, res, next) {
     console.warn(err.message)
     res.status(err.status || 500).send({ error: err.message })
+  },
+
+  hasRole(role) {
+    return (req, res, next) => {
+      if (role === req.user.role) {
+        return next()
+      } else {
+        let err = new Error('Unauthorized')
+        err.status = 401
+        return next(err)
+      }
+    }
   }
 }
--- routes/company_route.js
+++ routes/company_route.js
@@ -7,19 +7,10 @@ const passport = require('../services/passport')
 const requireSignin = passport.authenticate('local', { session: false })
 const requireAuth = passport.authenticate('jwt', { session: false })
 const router = require('express').Router()
-
-const onlyCompany = (req, res, next) => {
-  const role = req.user.role
-  if (role === 'company') {
-    return next()
-  }
-  let err = new Error('Unauthorized')
-  err.status = 401
-  return next(err)
-}
+const { hasRole } = require('../middlewares')
 
 router.post('/signup', signup)
 router.post('/signin', requireSignin, signin)
-router.get('/profile', requireAuth, onlyCompany, profile)
+router.get('/profile', requireAuth, hasRole('company'), profile)
 
 module.exports = router
