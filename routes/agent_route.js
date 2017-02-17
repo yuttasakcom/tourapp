b6c92af6e541d4ba24c72d1e6098c374ba356c7c
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-17 11:58:38 +0700

    add /signin route

--- routes/agent_route.js
+++ routes/agent_route.js
@@ -1,8 +1,10 @@
 const {
-  signup
+  signup,
+	signin
 } = require('../controllers/agent_controller')
 const router = require('express').Router()
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 module.exports = router
