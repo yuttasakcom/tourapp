dcf2b2bb3090f2e4ba6dcbf8b4de009ef4f9f15d
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-21 18:58:49 +0700

    add agent get accept pendings list feature

--- controllers/agent_controller.js
+++ controllers/agent_controller.js
@@ -111,4 +111,13 @@ module.exports = {
         res.send(requestPendings)
       })
   },
+
+  getAcceptPendingsList(req, res, next) {
+    const agentId = req.user._id
+
+    Agent.findById(agentId, { _id: 0, acceptPendings: 1 })
+      .then(acceptPendings => {
+        res.send(acceptPendings)
+      })
+  }
 }
--- test/controllers/agent_pendings_list_test.js
+++ test/controllers/agent_pendings_list_test.js
@@ -29,6 +29,8 @@ describe('Agent pendings list', () => {
     agent1 = new Agent(agent1Props)
     agent1.requestPendings.push(company1Stub)
     agent1.requestPendings.push(company2Stub)
+    agent1.acceptPendings.push(company1Stub)
+    agent1.acceptPendings.push(company2Stub)
 
     agent1.save()
       .then(() => {
@@ -53,6 +55,19 @@ describe('Agent pendings list', () => {
         if (err) return done(err)
 
         expect(res.body.requestPendings.length).to.equal(2)
+        done()
+      })
+  })
+
+  it('two company must appear on GET /agents/accept-pendings', done => {
+    request(app)
+      .get('/agents/accept-pendings')
+      .set('authorization', agent1Token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+
+        expect(res.body.acceptPendings.length).to.equal(2)
         done()
       })
   })
