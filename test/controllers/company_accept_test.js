801d4960c2a43f1254372ec6a158c085a07978d0
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-21 12:49:42 +0700

    add stub

---
+++ test/controllers/company_accept_test.js
@@ -0,0 +1,56 @@
+const app = require('../../app')
+const request = require('supertest')
+const expect = require('chai').expect
+const mongoose = require('mongoose')
+const Agent = mongoose.model('Agent')
+const Company = mongoose.model('Company')
+
+describe.only('Company accept', () => {
+
+  let company1, agent1, company1Token, agent1Token
+
+  const company1Props = {
+    email: 'company1@test.com',
+    password: '1234'
+  }
+
+  const agent1Props = {
+    email: 'agent1@test.com',
+    password: '1234'
+  }
+
+  const company1SigninProps = Object.assign({}, company1Props, { role: 'company' })
+  const agent1SigninProps = Object.assign({}, agent1Props, { role: 'agent' })
+
+  beforeEach(done => {
+    company1 = new Company(company1Props)
+    agent1 = new Agent(agent1Props)
+
+    Promise.all([
+        company1.save(),
+        agent1.save()
+      ])
+      .then(() => {
+        request(app)
+          .post('/companies/signin')
+          .send(company1SigninProps)
+          .end((err, res) => {
+            company1Token = res.body.token
+
+            request(app)
+              .post('/agents/signin')
+              .send(agent1SigninProps)
+              .end((err, res) => {
+                agent1Token = res.body.token
+                done()
+              })
+          })
+      })
+  })
+  
+  it('test', done => {
+  	console.log(company1Token)
+  	console.log(agent1Token)
+  	done()
+  })
+})
