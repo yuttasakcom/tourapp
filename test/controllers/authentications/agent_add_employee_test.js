801ebee5f02a96a5f52fa3adb95d3e67b45d688f
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-24 17:58:33 +0700

    fix test stub

--- test/controllers/authentications/agent_add_employee_test.js
+++ test/controllers/authentications/agent_add_employee_test.js
@@ -23,19 +23,26 @@ describe('Agent add employee', () => {
   const agent1SigninProps = Object.assign({}, agent1Props, { role: 'agent' })
 
   beforeEach(done => {
-    agent1 = new Agent(agent1Props)
-    agent1.save()
-      .then(() => {
-        request(app)
-          .post('/agents/signin')
-          .send(agent1SigninProps)
-          .end((err, res) => {
-            agent1Token = res.body.token
+    request(app)
+      .post('/agents/signup')
+      .send(agent1Props)
+      .end((err, res) => {
 
-            done()
+        Agent.findOne({ email: agent1Props.email })
+          .then(agent => {
+            agent1 = agent
+            request(app)
+              .post('/agents/signin')
+              .send(agent1SigninProps)
+              .end((err, res) => {
+                agent1Token = res.body.token
+
+                done()
+              })
           })
       })
   })
+
 
   it('one employee', done => {
     request(app)
