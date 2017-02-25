db5ddceb86561c88d8f0f0b0eea97c3254608310
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-25 11:55:47 +0700

    add signin middleware

--- routes/agent_employee_route.js
+++ routes/agent_employee_route.js
@@ -2,9 +2,13 @@ const {
   signin,
   profile
 } = require('../controllers/agent_employee_controller')
+const passport = require('../services/passport')
+const requireSignin = passport.authenticate('local', { session: false })
+const requireAuth = passport.authenticate('jwt', { session: false })
 const router = require('express').Router()
+const { hasRole } = require('../middlewares')
 
-router.post('/signin', signin)
+router.post('/signin', requireSignin, signin)
 router.get('/profile', profile)
 
 module.exports = router
