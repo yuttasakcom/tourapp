fa7ff20c6f3a332a0252f07bd957ab88763885fc
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-02-25 14:36:56 +0700

    add booking model test

---
+++ test/models/booking_model_test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect
+const mongoose = require('mongoose')
+const Booking = mongoose.model('Booking')
+
+describe.only('Booking model', () => {
+
+  it('add booking', done => {
+    const booking1 = new Booking({
+      agentId: '1',
+      employeeId: '2',
+      companyId: '3',
+      pkgId: '4',
+      tourist: {
+        name: 'Paiboon',
+        phoneNumber: '024283192',
+        hotel: 'clusterkit hotel',
+        adult: '3',
+        child: '1',
+        nationality: 'thai',
+        date: new Date(),
+        note: 'awesome trip'
+      }
+    })
+  })
+
+})
