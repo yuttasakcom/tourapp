164b44c4a70a32317a866a4e086017f65759cc5e
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-03-05 20:52:03 +0700

    change port to 4000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const logger = require('morgan')
 
 app.use(logger('dev'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
 	console.log('API listening on port:', port)
 })
