26a266ad055eff515a0f45bad692e020d8690d87
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-03-29 14:49:39 +0700

    refactor

--- server/src/services/passport.js
+++ server/src/services/passport.js
@@ -4,67 +4,69 @@ const Agent = require('../models/agent')
 const config = require('../config')
 const LocalStrategy = require('passport-local-roles')
 const passportJwt = require('passport-jwt')
+const { comparePassword } = require('../helpers/authentication')
+
 const JwtStrategy = passportJwt.Strategy
 const ExtractJwt = passportJwt.ExtractJwt
-const { comparePassword } = require('../helpers/authentication')
+
+const userCollection = role => {
+  switch (role) {
+    case 'company':
+      return Company
+    case 'agent':
+    case 'agentEmployee':
+      return Agent
+    default:
+      return Agent
+  }
+}
 
 const localLogin = new LocalStrategy({
   usernameField: 'email',
   roleField: 'role',
-  session: false
-}, function(email, password, role, done) {
-  const userCollection = (role) => {
-    switch (role) {
-      case 'company':
-        return Company
-      case 'agent':
-      case 'agentEmployee':
-        return Agent
-      default:
-        return Agent
-    }
-  }
-
+  session: false,
+}, (email, password, role, done) => {
   switch (role) {
     case 'company':
     case 'agent':
-      userCollection(role).findOne({ email: email }, {
+      userCollection(role)
+        .findOne({ email }, {
           password: 1,
-          email: 1
+          email: 1,
         })
         .then(user => {
           if (!user) return done(null, false)
 
-          comparePassword(password, user.password)
+          return comparePassword(password, user.password)
             .then(isMatch => {
               if (!isMatch) return done(null, false)
 
               return done(null, user)
             })
             .catch(done)
-
         })
         .catch(done)
       break
     case 'agentEmployee':
-      const emails = email.split('..')
-      const agentEmail = emails[0]
-      const employeeEmail = emails[1]
+      {
+        const emails = email.split('..')
+        const agentEmail = emails[0]
+        const employeeEmail = emails[1]
 
-      userCollection(role).findOne({ email: agentEmail }, {
+        userCollection(role)
+        .findOne({ email: agentEmail }, {
           employees: {
             $elemMatch: {
-              email: employeeEmail
-            }
+              email: employeeEmail,
+            },
           },
-          email: 1
+          email: 1,
         })
         .then(agent => {
           const user = agent.employees[0]
-
           if (!user) return done(null, false)
 
-          comparePassword(password, user.password)
+          return comparePassword(password, user.password)
             .then(isMatch => {
               if (!isMatch) return done(null, false)
 
@@ -73,7 +75,8 @@ const localLogin = new LocalStrategy({
             .catch(done)
         })
         .catch(done)
-      break
+        break
+      }
     default:
       done(null, false)
       break
@@ -82,10 +85,10 @@ const localLogin = new LocalStrategy({
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.secret
+  secretOrKey: config.secret,
 }
 
-const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
   done(null, payload)
 })
 
