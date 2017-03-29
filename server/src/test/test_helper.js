c3fee046694cf72aa4794c0014a2703379f6ed2c
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-03-29 19:35:13 +0700

    fix eslints for mocha

--- server/src/test/test_helper.js
+++ server/src/test/test_helper.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 before(done => {
-	mongoose.connect('mongodb://localhost/tourapp_test')
-	mongoose.connection
-		.once('open', () => done())
-		.on('error', err => console.warn('Warning', err))
+  mongoose.connect('mongodb://localhost/tourapp_test')
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => console.warn('Warning', err))
 })
 
 beforeEach(done => {
-	mongoose.connection.db.dropDatabase()
-		.then(() => done())
-		.catch(() => done())
+  mongoose.connection.db.dropDatabase()
+    .then(() => done())
+    .catch(() => done())
 })
