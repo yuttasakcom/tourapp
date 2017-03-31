c595a9bd4a51d30ba43db7c1be47e09fd25e7426
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2017-03-31 15:52:11 +0700

    setup mockgoose

--- server/test/test_helper.js
+++ server/test/test_helper.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose'
+import * as mongoose from 'mongoose'
+import { Mockgoose } from 'mockgoose'
+
+const mockgoose = new Mockgoose(mongoose)
 
 before(done => {
-  mongoose.connect('mongodb://localhost/tourapp_test')
-  mongoose.connection
-    .once('open', () => done())
-    .on('error', err => console.warn('Warning', err))
+  mockgoose.prepareStorage().then(() => {
+    console.log('prepare storage ok', mongoose.mocked)
+    mongoose.connect('mongodb://localhost/tourapp_test')
+    mongoose.connection
+      .once('open', () => done())
+      .on('error', err => console.warn('Warning', err))
+  })
 })
 
 beforeEach(done => {
