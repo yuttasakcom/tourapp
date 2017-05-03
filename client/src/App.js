daa7080a6ef0ffcaf26134a5dee10738d701bbd6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-05-03 13:59:20 +0700

    setup basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import SignIn from './components/SignIn'
 
 export default () => (
   <MuiThemeProvider>
-    <SignIn />
+    <Router>
+      <div>
+        <Route path="/signin" component={SignIn} />
+      </div>
+    </Router>
   </MuiThemeProvider>
 )
