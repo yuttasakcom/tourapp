9f07835cb9a6a6a7e5b67c0fbc9ccdd0bc559832
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-03 13:59:00 +0700

    setup basic react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import SignIn from './pages/signIn'
 
 class App extends Component {
   render() {
-    return <SignIn />
+    return (
+      <Router>
+        <div>
+          <Route path="/signin" component={SignIn} />
+          <Route path="/dashboard" component={SignIn} />
+        </div>
+      </Router>
+    )
   }
 }
 
