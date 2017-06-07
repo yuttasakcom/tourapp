d6e4525a3c010242699ce59f1eec7f7db6a1b815
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-07 15:35:22 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import ReduxThunk from 'redux-thunk'
+
+import reducers from './reducers'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
@@ -8,5 +13,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import './resources/css/material-kit.css'
 import './resources/css/material-dashboard.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const middlewares = composeEnhancers(applyMiddleware(ReduxThunk))
+const store = createStore(reducers, middlewares)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
