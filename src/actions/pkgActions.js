0a9b3c2cf0de5a4e99d163a2827a03d68dffaabf
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-10 08:58:08 +0700

    add delete package action

--- src/actions/pkgActions.js
+++ src/actions/pkgActions.js
@@ -2,7 +2,9 @@ import axios from './axios'
 import {
   FETCH_PKGS_SUCCESS,
   OPEN_ADD_PKG_MODAL,
-  CLOSE_ADD_PKG_MODAL
+  CLOSE_ADD_PKG_MODAL,
+  OPEN_DELETE_PKG_MODAL,
+  CLOSE_DELETE_PKG_MODAL
 } from './types'
 
 export const fetchPkgs = () => async dispatch => {
@@ -23,10 +25,27 @@ export const addPkg = pkg => async dispatch => {
   }
 }
 
+export const deletePkg = pkg => async dispatch => {
+  try {
+    const { data } = await axios.delete(`/companies/pkgs/${pkg._id}`)
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 export const openAddPkgModal = () => {
   return { type: OPEN_ADD_PKG_MODAL }
 }
 
 export const closeAddPkgModal = () => {
   return { type: CLOSE_ADD_PKG_MODAL }
+}
+
+export const openDeletePkgModal = () => {
+  return { type: OPEN_DELETE_PKG_MODAL }
+}
+
+export const closeDeletePkgModal = () => {
+  return { type: CLOSE_DELETE_PKG_MODAL }
 }
