04a8635070d2c24f2bcb479386f79685100d44dc
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-10 09:00:48 +0700

    add delete modal to reducer

--- src/reducers/ui/pkgReducer.js
+++ src/reducers/ui/pkgReducer.js
@@ -1,6 +1,11 @@
-import { OPEN_ADD_PKG_MODAL, CLOSE_ADD_PKG_MODAL } from '../../actions/types'
+import {
+  OPEN_ADD_PKG_MODAL,
+  CLOSE_ADD_PKG_MODAL,
+  OPEN_DELETE_PKG_MODAL,
+  CLOSE_DELETE_PKG_MODAL
+} from '../../actions/types'
 
-const initialState = { showAddPkgModal: false }
+const initialState = { showAddPkgModal: false, showDeletePkgModal: false }
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -9,6 +14,12 @@ export default (state = initialState, action) => {
 
     case CLOSE_ADD_PKG_MODAL:
       return { ...state, showAddPkgModal: false }
+
+    case OPEN_DELETE_PKG_MODAL:
+      return { ...state, showDeletePkgModal: true }
+
+    case CLOSE_DELETE_PKG_MODAL:
+      return { ...state, showDeletePkgModal: false }
 
     default:
       return state
