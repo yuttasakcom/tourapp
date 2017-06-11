ae72ff27abf11ab191e8a0cb5520905d4f74ae22
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-11 13:36:26 +0700

    setup agent datatable

---
+++ src/pages/manageAgent/DeleteModal.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Modal, Button } from 'react-bootstrap'
+
+import * as actions from '../../actions'
+
+class DeleteModal extends PureComponent {
+  onSubmit = agent => {
+    this.props.deleteAgent(agent)
+  }
+
+  render() {
+    const { showModal, closeDeleteAgentModal, deleteAgent, agent } = this.props
+
+    if (!agent) {
+      return <div />
+    }
+
+    return (
+      <Modal show={showModal} onHide={closeDeleteAgentModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Agent</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Are you sure to delete agent {agent.email} ?</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={closeDeleteAgentModal}>No</Button>
+          <Button bsStyle="danger" onClick={() => deleteAgent(agent)}>Yes</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
+
+const mapStateToProps = ({ agent }) => ({
+  showModal: agent.showDeleteAgentModal,
+  agent: agent.agents[agent.selectedAgent]
+})
+
+export default connect(mapStateToProps, actions)(DeleteModal)
