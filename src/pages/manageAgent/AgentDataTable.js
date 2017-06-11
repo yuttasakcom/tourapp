8385395e3d415fecc9919cc71dbbbe7c963be41d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-11 13:36:41 +0700

    setup agent datatable

---
+++ src/pages/manageAgent/AgentDataTable.js
@@ -0,0 +1,62 @@
+import _ from 'lodash'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+
+import Card from '../../components/Card'
+import DataTable from '../../components/dataTable'
+import DeleteModal from './DeleteModal'
+import * as actions from '../../actions'
+
+class AgentDataTable extends PureComponent {
+  componentDidMount() {
+    this.props.fetchAgents()
+  }
+
+  renderTableBody = () => {
+    const { agents, openDeleteAgentModal } = this.props
+
+    if (!agents) {
+      return <tr />
+    }
+
+    return _.map(agents, agent =>
+      <tr key={agent._id}>
+        <td>{agent._id}</td>
+        <td>{agent.email}</td>
+        <td style={{ textAlign: 'center' }}>
+          <button
+            className="btn btn-info btn-sm"
+            onClick={() => openDeleteAgentModal(agent._id)}
+          >
+            Contract Rate
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => openDeleteAgentModal(agent._id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    )
+  }
+
+  render() {
+    const tableTitles = ['_id', 'Email']
+    return (
+      <Card title="Agents" description="Manage agents">
+        <DataTable
+          tableTitles={tableTitles}
+          renderTableBody={this.renderTableBody}
+        />
+        <DeleteModal />
+      </Card>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  agents: state.agent.agents
+})
+
+export default connect(mapStateToProps, actions)(AgentDataTable)
