67a3ddf0e4acfaf8359a8838ab94eef7c4a25324
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-11 13:26:08 +0700

    setup request modal

---
+++ src/pages/manageAgent/RequestModal.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Modal } from 'react-bootstrap'
+
+import AgentForm from './AgentForm'
+import * as actions from '../../actions'
+
+class RequestModal extends PureComponent {
+  onSubmit = values => {
+    this.props.requestAgent(values)
+  }
+
+  render() {
+    const { showModal, closeRequestAgentModal } = this.props
+    return (
+      <Modal show={showModal} onHide={closeRequestAgentModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Request Agent</Modal.Title>
+        </Modal.Header>
+        <AgentForm onSubmit={this.onSubmit} closeModal={closeRequestAgentModal} />
+      </Modal>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  showModal: state.agent.showRequestAgentModal
+})
+
+export default connect(mapStateToProps, actions)(RequestModal)
