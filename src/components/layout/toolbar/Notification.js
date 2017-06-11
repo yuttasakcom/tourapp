d8b63b4b52b5a4c1234342b4a67a5ccd87769268
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-11 14:56:39 +0700

    open notificatoin on click

--- src/components/layout/toolbar/Notification.js
+++ src/components/layout/toolbar/Notification.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () =>
-  <li className="dropdown">
-    <a className="dropdown-toggle" data-toggle="dropdown">
-      <i className="material-icons">notifications</i>
-      <span className="notification">5</span>
-      <p className="hidden-lg hidden-md">Notifications</p>
-    </a>
-    <ul className="dropdown-menu">
-      <li><a>Mike John responded to your email</a></li>
-      <li><a>You have 5 new tasks</a></li>
-      <li><a>You're now friend with Andrew</a></li>
-      <li><a>Another Notification</a></li>
-      <li><a>Another One</a></li>
-    </ul>
-  </li>
+class Notification extends Component {
+  state = { open: false }
+
+  render() {
+    return (
+      <li className={`dropdown${this.state.open ? ' open' : ''}`}>
+        <a
+          style={{ cursor: 'pointer' }}
+          className="dropdown-toggle"
+          onClick={() => this.setState({ open: !this.state.open })}
+        >
+          <i className="material-icons">notifications</i>
+          <span className="notification">5</span>
+          <p className="hidden-lg hidden-md">Notifications</p>
+        </a>
+        <ul className="dropdown-menu">
+          <li><a>Mike John responded to your email</a></li>
+          <li><a>You have 5 new tasks</a></li>
+          <li><a>You're now friend with Andrew</a></li>
+          <li><a>Another Notification</a></li>
+          <li><a>Another One</a></li>
+        </ul>
+      </li>
+    )
+  }
+}
+
+export default Notification
