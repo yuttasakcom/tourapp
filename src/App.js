e62c36335f6ae66475cceeea5f9be17004406374
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 17:27:29 +0700

    setup auth and swich for signin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import { ConnectedRouter as Router } from 'connected-react-router'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
+import SignIn from './pages/signIn'
 import Dashboard from './pages/dashboard'
 import Notifications from './pages/notifications'
 import BoogkingSummary from './pages/bookingSummary'
@@ -13,19 +15,27 @@ import { history } from './store'
 
 class App extends PureComponent {
   render() {
+    console.log(this.props.authenticated)
     return (
       <Router history={history}>
-        <Layout>
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/notifications" component={Notifications} />
-          <Route path="/booking-summary" component={BoogkingSummary} />
-          <Route path="/manage-tour-package" component={ManageTourPkg} />
-          <Route path="/manage-agent" component={ManageAgent} />
-          <Route path="/contract-rate" component={ContractRate} />
-        </Layout>
+        <Switch>
+          <Route path="/signin" component={SignIn} />
+          <Layout>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/notifications" component={Notifications} />
+            <Route path="/booking-summary" component={BoogkingSummary} />
+            <Route path="/manage-tour-package" component={ManageTourPkg} />
+            <Route path="/manage-agent" component={ManageAgent} />
+            <Route path="/contract-rate" component={ContractRate} />
+          </Layout>
+        </Switch>
       </Router>
     )
   }
 }
 
-export default App
+const mapStateToProps = ({ auth: { authenticated } }) => {
+  return { authenticated }
+}
+
+export default connect(mapStateToProps)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 
+import auth from './authReducer'
 import pkg from './pkgReducer'
 import agent from './agentReducer'
 import notification from './notificationReducer'
 
 export default combineReducers({
   form,
+  auth,
   pkg,
   agent,
   notification
