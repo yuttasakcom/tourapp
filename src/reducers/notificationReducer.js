451636b345b0b294bd9755cd94d6e0d369a3775a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 11:32:41 +0700

    add request pending state to reducer

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -2,19 +2,28 @@ import {
   TOGGLE_NOTIFICATION_GEM,
   TOGGLE_ACCEPT_PENDING_GEM,
   TOGGLE_REQUEST_PENDING_GEM,
-  HIDE_ALL_GEM
+  HIDE_ALL_GEM,
+  FETCH_REQUEST_PENDINGS_SUCCESS
 } from '../actions/types'
 
 const initialState = {
   showNotificationGem: false,
   showRequestPendingGem: false,
-  showAcceptPendingGem: false
+  showAcceptPendingGem: false,
+  requestPendings: []
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_REQUEST_PENDINGS_SUCCESS:
+      return {
+        ...state,
+        requestPendings: action.payload
+      }
+
     case TOGGLE_NOTIFICATION_GEM:
       return {
+        ...state,
         showNotificationGem: !state.showNotificationGem,
         showRequestPendingGem: false,
         showAcceptPendingGem: false
@@ -22,6 +31,7 @@ export default (state = initialState, action) => {
 
     case TOGGLE_REQUEST_PENDING_GEM:
       return {
+        ...state,
         showRequestPendingGem: !state.showRequestPendingGem,
         showNotificationGem: false,
         showAcceptPendingGem: false
@@ -29,6 +39,7 @@ export default (state = initialState, action) => {
 
     case TOGGLE_ACCEPT_PENDING_GEM:
       return {
+        ...state,
         showAcceptPendingGem: !state.showAcceptPendingGem,
         showRequestPendingGem: false,
         showNotificationGem: false
@@ -36,6 +47,7 @@ export default (state = initialState, action) => {
 
     case HIDE_ALL_GEM:
       return {
+        ...state,
         showNotificationGem: false,
         showRequestPendingGem: false,
         showAcceptPendingGem: false
