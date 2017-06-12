7178db1b4d694c2aaeacabb10b53f052b7a786c5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 10:07:20 +0700

    setup notification action

---
+++ src/actions/notificationAction.js
@@ -0,0 +1,17 @@
+import {
+  TOGGLE_NOTIFICATION_GEM,
+  TOGGLE_ACCEPT_PENDING_GEM,
+  TOGGLE_REQUEST_PENDING_GEM
+} from './types'
+
+export const toggleNotificationGem = () => {
+  return { type: TOGGLE_NOTIFICATION_GEM }
+}
+
+export const toggleAcceptPendingGem = () => {
+  return { type: TOGGLE_ACCEPT_PENDING_GEM }
+}
+
+export const toggleRequestPendingGem = () => {
+  return { type: TOGGLE_REQUEST_PENDING_GEM }
+}
