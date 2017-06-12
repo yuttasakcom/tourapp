680ce86d6f1fcd4699aab47d11f30d6045e36fd5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 13:08:57 +0700

    refactor to pure component

--- src/components/layout/mainMenu/MenuItem.js
+++ src/components/layout/mainMenu/MenuItem.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 
-export default ({ text, icon, title, path }) =>
-  <li className={text === title ? 'active' : ''}>
-    <Link to={path}>
-      <i className="material-icons">{icon}</i>
-      <p>{text}</p>
-    </Link>
-  </li>
+class MenuItem extends PureComponent {
+  render() {
+    const { text, title, path, icon } = this.props
+
+    return (
+      <li className={text === title ? 'active' : ''}>
+        <Link to={path}>
+          <i className="material-icons">{icon}</i>
+          <p>{text}</p>
+        </Link>
+      </li>
+    )
+  }
+}
+
+export default MenuItem
