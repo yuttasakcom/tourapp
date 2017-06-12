259b8a01ee2aa474dd28e7d2e4b4769aed8ec719
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 13:52:53 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import App from './App'
 import store from './store'
 
 import 'font-awesome/css/font-awesome.min.css'
