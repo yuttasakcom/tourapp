611d0c5dc75345a2b277e0bfc2c1909d073e1c57
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-12 11:31:57 +0700

    add fetch request pending action

--- src/actions/notificationActions.js
+++ src/actions/notificationActions.js
@@ -1,9 +1,24 @@
+import axios from './axios'
+
 import {
   TOGGLE_NOTIFICATION_GEM,
   TOGGLE_ACCEPT_PENDING_GEM,
   TOGGLE_REQUEST_PENDING_GEM,
-  HIDE_ALL_GEM
+  HIDE_ALL_GEM,
+  FETCH_REQUEST_PENDINGS_SUCCESS
 } from './types'
+
+export const fetchRequestPendings = () => async dispatch => {
+  try {
+    const { data } = await axios.get('/companies/request-pendings')
+    dispatch({
+      type: FETCH_REQUEST_PENDINGS_SUCCESS,
+      payload: data.requestPendings
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 export const toggleNotificationGem = () => {
   return { type: TOGGLE_NOTIFICATION_GEM }
