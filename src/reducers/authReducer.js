f76e545781ec8019dac13a1522d8928e64c35299
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-13 12:43:53 +0700

    setup auth reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,9 +1,17 @@
+import { SIGN_IN_SUCCESS, SIGN_OUT_SUCCESS } from '../actions/types'
+
 const initialState = {
   authenticated: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SIGN_IN_SUCCESS:
+      return { authenticated: true }
+
+    case SIGN_OUT_SUCCESS:
+      return { authenticated: false }
+      
     default:
       return state
   }
