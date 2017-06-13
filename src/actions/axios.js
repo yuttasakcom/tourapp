ba6d96532151305e2da7800aa9170cfbcc17c96b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-13 15:49:58 +0700

    use real token instend

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   baseURL: 'http://localhost:4000'
 })
 
-instance.defaults.headers.common['Authorization'] =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OTNjZGY0MjQ2MzUzODI2MWM0MzdiOWMiLCJzdWIiOiJwYWlib29uMTU3MjFAZ21haWwuY29tIiwicm9sZSI6ImNvbXBhbnkiLCJpYXQiOjE0OTcxNjE1MzkwMDR9.p402VeOXLit8QrBe5klL31Cxsh9hP13FKgbbvN1Y2Eg'
+instance.defaults.headers.common['Authorization'] = localStorage.getItem(
+  'token'
+)
 export default instance
