5c93dd19b843b2aa7726bb056a55a754618a37aa
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-13 15:50:26 +0700

    implement auth action

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,13 +1,23 @@
+import axios from './axios'
+
 import { SIGN_IN_SUCCESS, SIGN_OUT_SUCCESS, SIGN_UP_SUCCESS } from './types'
 
-export const signIn = () => {
-  return { type: SIGN_IN_SUCCESS }
+export const signIn = values => async dispatch => {
+  const { data: { token } } = await axios.post('/companies/signin', {
+    ...values,
+    role: 'company'
+  })
+  localStorage.setItem('token', token)
+  dispatch({ type: SIGN_IN_SUCCESS })
 }
 
 export const signOut = () => {
+  localStorage.removeItem('token')
   return { type: SIGN_OUT_SUCCESS }
 }
 
-export const signUp = () => {
+export const signUp = values => async dispatch => {
+  const { data: { token } } = await axios.post('/companies/signup', values)
+  localStorage.setItem('token', token)
   return { type: SIGN_UP_SUCCESS }
 }
