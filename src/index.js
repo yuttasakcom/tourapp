d5fa98f7323855c1e086281cc7e71ce4994ac9b3
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-13 13:12:23 +0700

    dispatch sign in success for dev

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 import App from './App'
 import store from './store'
+import { SIGN_IN_SUCCESS } from './actions/types'
 
 import 'font-awesome/css/font-awesome.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import './resources/css/material-kit.css'
 import './resources/css/material-dashboard.css'
+
+store.dispatch({ type: SIGN_IN_SUCCESS })
 
 ReactDOM.render(
   <Provider store={store}>
