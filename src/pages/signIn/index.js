de559cdfa581430786d7aca9698048c654719ee9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-13 13:01:02 +0700

    mock signin component

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 import Header from './Header'
 import Body from './Body'
 import Footer from './Footer'
 import imgBackground from '../../resources/img/city.jpg'
+import * as actions from '../../actions'
 
-export default () => (
-  <div className="signup-page">
-    <Header />
-    <div className="wrapper">
-      <div className="header header-filter" style={styles.main}>
-        <Body />
-        <Footer />
+class SignIn extends PureComponent {
+  render() {
+    const { from } = this.props.location.state || {
+      from: { pathname: '/dashboard' }
+    }
+
+    if (this.props.authenticated) {
+      return <Redirect to={from} />
+    }
+
+    return (
+      <div className="signup-page">
+        <Header />
+        <div className="wrapper">
+          <div className="header header-filter" style={styles.main}>
+            <Body onSignIn={this.props.signIn} />
+            <Footer />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
+const mapStateToProps = ({ auth: { authenticated } }) => {
+  return { authenticated }
+}
+
+export default connect(mapStateToProps, actions)(SignIn)
 
 const styles = {
   main: {
