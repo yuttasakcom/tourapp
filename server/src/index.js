f0309757d46b023a45fbe1bd11c5217f115c3fa6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-18 11:24:23 +0700

    remove useless middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,5 @@
-import morgan from 'morgan'
 import app from './app'
 import logger from './utils/logger'
-
-app.use(morgan('dev'))
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
