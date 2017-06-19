167e9a9c7a836ac216f9e754bc453922507498fd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-19 09:47:03 +0700

    add booking modal reducer

--- src/reducers/bookingReducer.js
+++ src/reducers/bookingReducer.js
@@ -1,14 +1,25 @@
 import _ from 'lodash'
-import { FETCH_PKGS_SUCCESS } from '../actions/types'
+import {
+  FETCH_PKGS_SUCCESS,
+  OPEN_ADD_BOOKING_MODAL,
+  CLOSE_ADD_BOOKING_MODAL
+} from '../actions/types'
 
 const initialState = {
-  pkgs: {}
+  pkgs: {},
+  showAddBookingModal: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_PKGS_SUCCESS:
       return { ...state, pkgs: _.mapKeys(action.payload, '_id') }
+
+    case OPEN_ADD_BOOKING_MODAL:
+      return { ...state, showAddBookingModal: true }
+
+    case CLOSE_ADD_BOOKING_MODAL:
+      return { ...state, showAddBookingModal: false }
 
     default:
       return state
