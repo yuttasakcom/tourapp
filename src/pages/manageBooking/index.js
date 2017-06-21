594c68f0f025ca9b7a813f3f68f60304d1ee7d41
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-21 08:08:36 +0700

    setup manage booking route

---
+++ src/pages/manageBooking/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+class ManageBooking extends PureComponent {
+  render() {
+    return (
+      <div>Manage Booking</div>
+    )
+  }
+}
+
+export default ManageBooking
