f1fddb623b47c6f4fbcdfff1f32457465cbe83b7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-21 08:32:15 +0700

    setup booking datatable

---
+++ src/pages/manageBooking/BookingDataTable.js
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+
+import Card from '../../components/Card'
+import DataTable from '../../components/dataTable'
+import * as actions from '../../actions'
+
+class BookingDataTable extends PureComponent {
+  componentDidMount() {
+    this.props.fetchBookings()
+  }
+
+  renderTableBody = () => {
+    const { bookings } = this.props
+
+    if (!bookings) {
+      return <tr />
+    }
+
+    return _.map(bookings, booking =>
+      <tr key={booking._id}>
+        <td>{booking.agent.email}</td>
+        <td>{booking.pkg.name}</td>
+        <td>{booking.tourist.name}</td>
+        <td>{booking.tourist.adult}</td>
+        <td>{booking.tourist.child}</td>
+      </tr>
+    )
+  }
+
+  render() {
+    const tableTitles = ['Agent', 'Package', 'Tourist', 'Adult', 'Child']
+    return (
+      <Card title="Bookings" description="Manage booking">
+        <DataTable
+          tableTitles={tableTitles}
+          renderTableBody={this.renderTableBody}
+        />
+      </Card>
+    )
+  }
+}
+
+const mapStateToProps = ({ booking: { bookings } }) => ({
+  bookings
+})
+
+export default connect(mapStateToProps, actions)(BookingDataTable)
--- src/pages/manageBooking/index.js
+++ src/pages/manageBooking/index.js
@@ -1,9 +1,17 @@
 import React, { PureComponent } from 'react'
 
+import BookingDataTable from './BookingDataTable'
+
 class ManageBooking extends PureComponent {
   render() {
     return (
-      <div>Manage Booking</div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <BookingDataTable />
+          </div>
+        </div>
+      </div>
     )
   }
 }
