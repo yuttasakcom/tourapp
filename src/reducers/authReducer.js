132815c6a642038f0a623711783a4fe2f94ac35f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-22 20:59:20 +0700

    add toggle profile menu state

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,17 +4,26 @@ import {
   SIGN_UP_SUCCESS,
   SIGN_IN_FAIL,
   SIGN_UP_FAIL,
-  HIDE_AUTH_NOTIFICATION
+  HIDE_AUTH_NOTIFICATION,
+  TOGGLE_PROFILE_MENU,
+  HIDE_ALL_GEM
 } from '../actions/types'
 
 const initialState = {
   authenticated: false,
   _id: null,
+  showProfileMenu: false,
   notification: { show: false, type: null, message: null }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case HIDE_ALL_GEM:
+      return { ...state, showProfileMenu: false }
+
+    case TOGGLE_PROFILE_MENU:
+      return { ...state, showProfileMenu: !state.showProfileMenu }
+
     case SIGN_IN_SUCCESS:
       return { ...state, authenticated: true, _id: action.payload }
 
