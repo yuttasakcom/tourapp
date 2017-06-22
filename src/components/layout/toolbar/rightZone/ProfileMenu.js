b8fd89682e8c2689a3a8c90741a8e303389433b0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-22 20:38:22 +0700

    profile menu must show qrcode and _id

--- src/components/layout/toolbar/rightZone/ProfileMenu.js
+++ src/components/layout/toolbar/rightZone/ProfileMenu.js
@@ -1,12 +1,14 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 
+import QRCode from 'qrcode.react'
 import * as actions from '../../../../actions'
 
 class ProfileMenu extends PureComponent {
   render() {
+    const { _id } = this.props
     return (
-      <li>
+      <li className="dropdown open">
         <a
           style={{ cursor: 'pointer' }}
           onClick={this.props.signOut}
@@ -15,9 +17,17 @@ class ProfileMenu extends PureComponent {
           <i className="material-icons">person</i>
           <p className="hidden-lg hidden-md">Profile</p>
         </a>
+        <ul className="dropdown-menu">
+          <li><a><QRCode value={_id} /></a></li>
+          <li><a>{_id}</a></li>
+        </ul>
       </li>
     )
   }
 }
 
-export default connect(null, actions)(ProfileMenu)
+const mapStateToProps = ({ auth: { _id } }) => {
+  return { _id }
+}
+
+export default connect(mapStateToProps, actions)(ProfileMenu)
