12f88f5e9365592ede5027195719c2a3443d7560
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-23 08:15:52 +0700

    set profile menu and _id reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,24 +4,34 @@ import {
   SIGN_UP_SUCCESS,
   SIGN_IN_FAIL,
   SIGN_UP_FAIL,
-  HIDE_AUTH_NOTIFICATION
+  HIDE_AUTH_NOTIFICATION,
+  TOGGLE_PROFILE_MENU,
+  HIDE_ALL_GEM
 } from '../actions/types'
 
 const initialState = {
   authenticated: false,
+  _id: null,
+  showProfileMenu: false,
   notification: { show: false, type: null, message: null }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case HIDE_ALL_GEM:
+      return { ...state, showProfileMenu: false }
+
+    case TOGGLE_PROFILE_MENU:
+      return { ...state, showProfileMenu: !state.showProfileMenu }
+
     case SIGN_IN_SUCCESS:
-      return { ...state, authenticated: true }
+      return { ...state, authenticated: true, _id: action.payload }
 
     case SIGN_IN_FAIL:
       return { ...state, notification: { ...action.payload, show: true } }
 
     case SIGN_UP_SUCCESS:
-      return { ...state, authenticated: true }
+      return { ...state, authenticated: true, _id: action.payload }
 
     case SIGN_UP_FAIL:
       return { ...state, notification: { ...action.payload, show: true } }
