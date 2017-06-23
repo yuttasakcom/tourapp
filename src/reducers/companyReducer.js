40837dfcfa179f81379be1a85052a9b2cf24d988
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-23 22:02:26 +0700

    add fetch companies reducer

--- src/reducers/companyReducer.js
+++ src/reducers/companyReducer.js
@@ -1,14 +1,20 @@
+import _ from 'lodash'
 import {
   OPEN_REQUEST_COMPANY_MODAL,
-  CLOSE_REQUEST_COMPANY_MODAL
+  CLOSE_REQUEST_COMPANY_MODAL,
+  FETCH_COMPANIES_SUCCESS
 } from '../actions/types'
 
 const initialState = {
+  companies: {},
   showRequestCompanyModal: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_COMPANIES_SUCCESS:
+      return { ...state, companies: _.mapKeys(action.payload, '_id') }
+
     case OPEN_REQUEST_COMPANY_MODAL:
       return { ...state, showRequestCompanyModal: true }
 
