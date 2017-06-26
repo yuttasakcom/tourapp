a93c5f080bcbb48518bd6dc442c2c927c572bc6e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-26 12:02:03 +0700

    add socket io  on request

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -16,18 +16,20 @@ export default io => {
     const { _id, sub, role } = socket.decoded_token
     logger.info(`${role} ${sub} has connected!`)
     await redis.lpushAsync(`socketId:${_id}`, socket.id)
+    await redis.hmsetAsync(`userDetail:${socket.id}`, { _id, email: sub, role })
 
-    socket.on('message', data => {
-      socket.broadcast.emit('message', data)
-    })
-
-    socket.on('book', data => {
-      socket.broadcast.emit(data.company, data)
+    socket.on('request', async data => {
+      const resSockets = await redis.lrangeAsync(`socketId:${data._id}`, 0, -1)
+      const resUserDetail = await redis.hgetallAsync(`userDetail:${socket.id}`)
+      resSockets.forEach(resSocket => {
+        io.to(resSocket).emit('request', resUserDetail)
+      })
     })
 
     socket.on('disconnect', async () => {
       logger.info(`${role} ${sub} has disconnected!`)
       await redis.lremAsync(`socketId:${_id}`, 0, socket.id)
+      await redis.delAsync(`userDetail:${socket.id}`)
     })
   })
 }
