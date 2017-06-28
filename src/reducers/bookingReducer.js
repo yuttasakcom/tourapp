f5de6df67228a1cee274d81bc7dba7e9f2bdce93
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-28 20:52:03 +0700

    add manage modal reducer

--- src/reducers/bookingReducer.js
+++ src/reducers/bookingReducer.js
@@ -3,11 +3,15 @@ import _ from 'lodash'
 import { waiting } from '../actions/bookingStatus'
 import {
   FETCH_BOOKINGS_SUCCESS,
-  SET_BOOKINGS_VISIBILITY_FILTER
+  SET_BOOKINGS_VISIBILITY_FILTER,
+  OPEN_MANAGE_BOOKING_MODAL,
+  CLOSE_MANAGE_BOOKING_MODAL
 } from '../actions/types'
 
 const initialState = {
   bookings: {},
+  selectedBooking: null,
+  showManageBookingModal: false,
   visibilityFilter: waiting
 }
 
@@ -18,6 +22,16 @@ export default (state = initialState, action) => {
 
     case SET_BOOKINGS_VISIBILITY_FILTER:
       return { ...state, visibilityFilter: action.payload }
+
+    case OPEN_MANAGE_BOOKING_MODAL:
+      return {
+        ...state,
+        showManageBookingModal: true,
+        selectedBooking: action.payload
+      }
+
+    case CLOSE_MANAGE_BOOKING_MODAL:
+      return { ...state, showManageBookingModal: false }
 
     default:
       return state
