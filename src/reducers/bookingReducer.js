3d32ecd0e018c57bb2d5e9a765ef880e545d4d57
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-28 19:29:02 +0700

    add set booking visibility filter reducer

--- src/reducers/bookingReducer.js
+++ src/reducers/bookingReducer.js
@@ -1,14 +1,23 @@
 import _ from 'lodash'
-import { FETCH_BOOKINGS_SUCCESS } from '../actions/types'
+
+import bookingStatus from '../actions/bookingStatus'
+import {
+  FETCH_BOOKINGS_SUCCESS,
+  SET_BOOKINGS_VISIBILITY_FILTER
+} from '../actions/types'
 
 const initialState = {
-  bookings: {}
+  bookings: {},
+  visibilityFilter: bookingStatus.waiting
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKINGS_SUCCESS:
       return { ...state, bookings: _.mapKeys(action.payload, '_id') }
+
+    case SET_BOOKINGS_VISIBILITY_FILTER:
+      return { ...state, visibilityFilter: action.payload }
 
     default:
       return state
