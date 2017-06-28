6516d3f7d2a9683b871aea3c1d7f10220bfa6068
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-28 11:51:22 +0700

    add datepicker component

---
+++ src/components/DatePicker.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react'
+import { SingleDatePicker } from 'react-dates'
+
+import 'react-dates/lib/css/_datepicker.css'
+
+class DatePicker extends PureComponent {
+  state = {
+    focused: false
+  }
+
+  render() {
+    const { date, onDateChange } = this.props
+    return (
+      <SingleDatePicker
+        date={date}
+        onDateChange={onDateChange}
+        focused={this.state.focused}
+        onFocusChange={({ focused }) => this.setState({ focused })}
+      />
+    )
+  }
+}
+
+export default DatePicker
