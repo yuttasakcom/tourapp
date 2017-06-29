359c486752ee7f1ac6dd30858104787aa4464b87
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-29 15:25:52 +0700

    add manage booking reducer

--- src/reducers/manageBookingReducer.js
+++ src/reducers/manageBookingReducer.js
@@ -1,15 +1,34 @@
 import _ from 'lodash'
 
-import { FETCH_BOOKINGS_SUCCESS } from '../actions/types'
+import {
+  FETCH_BOOKINGS_SUCCESS,
+  OPEN_MANAGE_BOOKING_MODAL,
+  CLOSE_MANAGE_BOOKING_MODAL
+} from '../actions/types'
 
 const initialState = {
-  bookings: {}
+  bookings: {},
+  selectedBooking: null,
+  showManageBookingModal: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKINGS_SUCCESS:
       return { ...state, bookings: _.mapKeys(action.payload, '_id') }
+
+    case OPEN_MANAGE_BOOKING_MODAL:
+      return {
+        ...state,
+        showManageBookingModal: true,
+        selectedBooking: action.payload
+      }
+
+    case CLOSE_MANAGE_BOOKING_MODAL:
+      return {
+        ...state,
+        showManageBookingModal: false
+      }
 
     default:
       return state
