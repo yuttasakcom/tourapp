46b544cad49e71bc90b50941a43a4a183da77c2a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-29 16:09:19 +0700

    add set bookings visibility filter reducer

--- src/reducers/manageBookingReducer.js
+++ src/reducers/manageBookingReducer.js
@@ -1,21 +1,27 @@
 import _ from 'lodash'
 
+import { waiting } from '../actions/bookingStatus'
 import {
   FETCH_BOOKINGS_SUCCESS,
   OPEN_MANAGE_BOOKING_MODAL,
-  CLOSE_MANAGE_BOOKING_MODAL
+  CLOSE_MANAGE_BOOKING_MODAL,
+  SET_BOOKINGS_VISIBILITY_FILTER
 } from '../actions/types'
 
 const initialState = {
   bookings: {},
   selectedBooking: null,
-  showManageBookingModal: false
+  showManageBookingModal: false,
+  visibilityFilter: waiting
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKINGS_SUCCESS:
       return { ...state, bookings: _.mapKeys(action.payload, '_id') }
+
+    case SET_BOOKINGS_VISIBILITY_FILTER:
+      return { ...state, visibilityFilter: action.payload }
 
     case OPEN_MANAGE_BOOKING_MODAL:
       return {
