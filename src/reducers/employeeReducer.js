09fb46992644453d79456aa3585e32b5cd199b5e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-30 13:55:40 +0700

    add fetch employees reducer

--- src/reducers/employeeReducer.js
+++ src/reducers/employeeReducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import { FETCH_EMPLOYEES_SUCCESS } from '../actions/types'
 
 const initialState = {
   employees: {},
@@ -10,6 +11,9 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_EMPLOYEES_SUCCESS:
+      return { ...state, employees: _.mapKeys(action.payload, '_id') }
+      
     default:
       return state
   }
