4e271bb24ff60e46053cdfcb98c59f16a7102aec
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-06-30 14:03:39 +0700

    add employee modal reducer

--- src/reducers/employeeReducer.js
+++ src/reducers/employeeReducer.js
@@ -1,5 +1,9 @@
 import _ from 'lodash'
-import { FETCH_EMPLOYEES_SUCCESS } from '../actions/types'
+import {
+  FETCH_EMPLOYEES_SUCCESS,
+  OPEN_ADD_EMPLOYEE_MODAL,
+  CLOSE_ADD_EMPLOYEE_MODAL
+} from '../actions/types'
 
 const initialState = {
   employees: {},
@@ -13,7 +17,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_EMPLOYEES_SUCCESS:
       return { ...state, employees: _.mapKeys(action.payload, '_id') }
-      
+
+    case OPEN_ADD_EMPLOYEE_MODAL:
+      return { ...state, showAddEmployeeModal: true }
+
+    case CLOSE_ADD_EMPLOYEE_MODAL:
+      return { ...state, showAddEmployeeModal: false }
+
     default:
       return state
   }
