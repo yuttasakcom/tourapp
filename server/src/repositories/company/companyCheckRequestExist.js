0a1e8362467df90fe2ecc68bda583a7d5d6e35cb
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-11 16:57:24 +0700

    fix bug

--- server/src/repositories/company/companyCheckRequestExist.js
+++ server/src/repositories/company/companyCheckRequestExist.js
@@ -1,7 +1,7 @@
 const Company = require('../../models/company')
 
 module.exports = async (companyId, agentId) => {
-  const { nModified } = Company.update(
+  const { nModified } = await Company.update(
     { _id: companyId },
     {
       $pull: { acceptPendings: agentId }
