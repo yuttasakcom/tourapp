c44cdb8c8a96dc9eeb7fd0a4c3d6ec1a3deb7d54
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-11 14:23:22 +0700

    refactor

--- server/src/controllers/company/accept.js
+++ server/src/controllers/company/accept.js
@@ -1,18 +1,14 @@
+const repo = require('../../repositories')
 const Company = require('../../models/company')
 const Agent = require('../../models/agent')
 
 module.exports = async (req, res, next) => {
-  const agentId = req.body._id
   const companyId = req.user._id
+  const agentId = req.body._id
 
-  const { nModified } = await Company.update(
-    { _id: companyId },
-    {
-      $pull: { acceptPendings: agentId }
-    }
-  )
+  const exist = await repo.companyCheckRequestExist(companyId, agentId)
 
-  if (!nModified) {
+  if (!exist) {
     const err = new Error('Request not found')
     err.status = 422
     return next(err)
---
+++ server/src/repositories/company/index.js
@@ -0,0 +1,1 @@
+exports.companyCheckRequestExist = require('./companyCheckRequestExist')
