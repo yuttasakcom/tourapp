aec78b40d4f9aa9cf7be5ffebd27a1fa17153203
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-14 15:58:36 +0700

    add env host for redis

--- server/src/socket/redis.js
+++ server/src/socket/redis.js
@@ -4,4 +4,6 @@ const bluebird = require('bluebird')
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
 
-module.exports = redis.createClient()
+module.exports = redis.createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1'
+})
