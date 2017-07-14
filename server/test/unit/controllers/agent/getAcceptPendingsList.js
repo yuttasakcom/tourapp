8ba9c19ea380358e581841f5a77d75482d9988b5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-14 14:39:36 +0700

    add testcase

--- server/test/unit/controllers/agent/getAcceptPendingsList.js
+++ server/test/unit/controllers/agent/getAcceptPendingsList.js
@@ -1,5 +1,38 @@
 const sinon = require('sinon')
 
+const getAcceptPendingsList = require('../../../../src/controllers/agent/getAcceptPendingsList')
+const repo = require('../../../../src/repositories')
+
 describe('agent getAcceptPendingsList controller', () => {
-   
+  const req = { user: { _id: 'agentId' } }
+  const res = { send: () => '' }
+  let resSendStub
+  let agentGetAcceptPendingsListStub
+
+  beforeEach(() => {
+    resSendStub = sinon.spy(res, 'send')
+    agentGetAcceptPendingsListStub = sinon
+      .stub(repo, 'agentGetAcceptPendingsList')
+      .resolves(true)
+  })
+
+  it('agentGetAcceptPendingsList must be called once', async () => {
+    await getAcceptPendingsList(req, res)
+    sinon.assert.calledOnce(agentGetAcceptPendingsListStub)
+  })
+
+  it('agentGetAcceptPendingsList must be called with agentId', async () => {
+    await getAcceptPendingsList(req, res)
+    sinon.assert.calledWith(agentGetAcceptPendingsListStub, 'agentId')
+  })
+
+  it('getAcceptPendingsList complete must be return data', async () => {
+    await getAcceptPendingsList(req, res)
+    sinon.assert.calledWith(resSendStub, true)
+  })
+
+  afterEach(() => {
+    resSendStub.restore()
+    agentGetAcceptPendingsListStub.restore()
+  })
 })
