f33ac13c969f2dccb29217cea81a1605acfc0217
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-16 10:42:27 +0700

    booking can populate agent name

--- server/src/repositories/agent/agentAddBooking.js
+++ server/src/repositories/agent/agentAddBooking.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose')
+
 const Booking = require('../../models/booking')
 
-module.exports = bookingProps => Booking.create(bookingProps)
+module.exports = bookingProps =>
+  Booking.findOneAndUpdate({ _id: mongoose.Types.ObjectId() }, bookingProps, {
+    new: true,
+    upsert: true,
+    runValidators: true,
+    populate: { path: 'agent', select: 'name' }
+  })
