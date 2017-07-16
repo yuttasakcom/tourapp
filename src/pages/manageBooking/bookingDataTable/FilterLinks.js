4b90280e98e1f6d27c8751046c5edb5fcb3d9f36
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-16 10:07:25 +0700

    colorize filterlink

--- src/pages/manageBooking/bookingDataTable/FilterLinks.js
+++ src/pages/manageBooking/bookingDataTable/FilterLinks.js
@@ -19,24 +19,28 @@ class FilterLinks extends PureComponent {
           <FilterLink
             icon="alarm"
             text="Waiting"
+            style={{ color: '#5882FA' }}
             active={visibilityFilter === waiting}
             onClick={() => setBookingsVisibilityFilter(waiting)}
           />
           <FilterLink
             icon="done_all"
             text="Accepted"
+            style={{ color: '#FF8000' }}
             active={visibilityFilter === accepted}
             onClick={() => setBookingsVisibilityFilter(accepted)}
           />
           <FilterLink
             icon="check_circle"
             text="Completed"
+            style={{ color: '#3ADF00' }}
             active={visibilityFilter === completed}
             onClick={() => setBookingsVisibilityFilter(completed)}
           />
           <FilterLink
             icon="clear"
             text="Rejected"
+            style={{ color: '#FF0040' }}
             active={visibilityFilter === rejected}
             onClick={() => setBookingsVisibilityFilter(rejected)}
           />
