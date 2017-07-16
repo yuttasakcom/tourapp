5580ec560aeed11faffeb054dd1057fedbb3e81a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-16 12:51:41 +0700

    fix typo

--- server/src/socket/handler/onBookingStatusUpdate.js
+++ server/src/socket/handler/onBookingStatusUpdate.js
@@ -1,7 +1,7 @@
 const { getSocketIds } = require('../helper')
 
 module.exports = async (socket, data) => {
-  const socketIds = await getSocketIds(data.agent._id)
+  const socketIds = await getSocketIds(data.agent)
   socketIds.forEach(socketId => {
     socket.broadcast.to(socketId).emit('bookingStatusUpdate', data)
   })
