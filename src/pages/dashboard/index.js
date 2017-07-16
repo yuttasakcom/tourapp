4ea9888c97a47268a6fb3e7e0030abde9d30eabe
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-16 09:52:51 +0700

    add dashboard for show

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,35 +1,12 @@
 import React, { PureComponent } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+
+import dashboard from '../../resources/images/dashboard.png'
 
 class Dashboard extends PureComponent {
-  state = { showModal: false }
-
-  close = () => {
-    this.setState({ showModal: false })
-  }
-
-  open = () => {
-    this.setState({ showModal: true })
-  }
-
   render() {
     return (
       <div>
-        <Button bsStyle="primary" bsSize="large" onClick={this.open}>
-          Launch demo modal
-        </Button>
-
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Body</h4>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
+        <img src={dashboard} alt="dashboard" />
       </div>
     )
   }
