bfe7237cf8a40e1e636def03b4a903420c991d5f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-23 10:06:44 +0700

    fix timestamp format

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,10 +1,11 @@
 const winston = require('winston')
+const moment = require('moment')
 
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       colorize: true,
-      timestamp: true,
+      timestamp: () => moment().format('DD/MM/YYYY hh:mm:ss'),
       prettyPrint: true,
       label: 'tourapp-api'
     })
