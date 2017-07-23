c45f04ab9b3ad00900cbc48524c4b6bf152336b4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-23 09:22:18 +0700

    add close menu reducer

--- client/agent-web/src/reducers/layoutReducer.js
+++ client/agent-web/src/reducers/layoutReducer.js
@@ -1,4 +1,4 @@
-import { TOGGLE_MENU } from '../actions/types'
+import { TOGGLE_MENU, CLOSE_MENU } from '../actions/types'
 
 const initialState = {
   showMenu: false
@@ -8,6 +8,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_MENU:
       return { ...state, showMenu: !state.showMenu }
+
+    case CLOSE_MENU:
+      return { ...state, showMenu: false }
+
     default:
       return state
   }
