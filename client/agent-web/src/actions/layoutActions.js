f0d399d978dd89df7893fda2d07c1a08e8c26729
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-23 09:17:00 +0700

    add closeMenu action

--- client/agent-web/src/actions/layoutActions.js
+++ client/agent-web/src/actions/layoutActions.js
@@ -1,7 +1,13 @@
-import { TOGGLE_MENU } from './types'
+import { TOGGLE_MENU, CLOSE_MENU } from './types'
 
 export const toggleMenu = () => {
   return {
     type: TOGGLE_MENU
+  }
+}
+
+export const closeMenu = () => {
+  return {
+    type: CLOSE_MENU
   }
 }
