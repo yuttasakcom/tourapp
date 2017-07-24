bffb74eaac292ea600f4673a00b56e0f7e48aa7f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-24 11:08:51 +0700

    fix height

--- client/company-web/src/components/Card.js
+++ client/company-web/src/components/Card.js
@@ -3,10 +3,14 @@ import React from 'react'
 export default ({ title, description, children }) =>
   <div className="card">
     <div className="card-header" data-background-color="purple">
-      <h4 className="title">{title}</h4>
-      <p className="category">{description}</p>
+      <h4 className="title">
+        {title}
+      </h4>
+      <p className="category">
+        {description}
+      </p>
     </div>
-    <div className="card-content table-responsive">
+    <div className="card-content table-responsive" style={{ height: 400 }}>
       {children}
     </div>
   </div>
