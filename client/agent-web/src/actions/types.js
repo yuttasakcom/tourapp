9c74a8f410a86c1d65f933034282b5f921dd1e85
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-07-28 10:01:13 +0700

    add date filter type

--- client/agent-web/src/actions/types.js
+++ client/agent-web/src/actions/types.js
@@ -23,6 +23,7 @@ export const FETCH_BOOKINGS_SUCCESS = 'fetch_bookings_success'
 export const OPEN_MANAGE_BOOKING_MODAL = 'open_manage_booking_modal'
 export const CLOSE_MANAGE_BOOKING_MODAL = 'close_manage_booking_modal'
 export const SET_BOOKINGS_STATUS_VISIBILITY_FILTER = 'set_bookings_status_visibility_filter'
+export const SET_BOOKINGS_DATE_VISIBILITY_FILTER = 'set_bookings_date_visibility_filter'
 
 export const FETCH_COMPANIES_SUCCESS = 'fetch_companies_success'
 export const REQUEST_COMPANY_SUCCESS = 'request_company_success'
