c0d124bbf77c78e18a9d96d57bd245733a0171c3
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-01 18:43:54 +0700

    refactor

---
+++ client/company-web/src/pages/manageBooking/bookingDataTable/Table.js
@@ -0,0 +1,97 @@
+import { filter } from 'lodash'
+import React, { PureComponent } from 'react'
+import flat from 'flat'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { connect } from 'react-redux'
+
+import * as actions from '../../../actions'
+import { waiting, readed } from '../../../actions/bookingStatus'
+
+class Table extends PureComponent {
+  componentDidMount() {
+    this.props.fetchBookings(this.props.date)
+  }
+
+  renderViewButton = (cell, row) => {
+    const { openManageBookingModal } = this.props
+    return (
+      <button
+        className="btn btn-info btn-sm"
+        style={{ margin: 0 }}
+        onClick={() => openManageBookingModal(row._id, row.status)}
+      >
+        View
+      </button>
+    )
+  }
+
+  render() {
+    const { bookings } = this.props
+    return (
+      <BootstrapTable
+        data={bookings}
+        height={340}
+        exportCSV
+        search
+        scrollTop="Bottom"
+        options={{ clearSearch: true }}
+      >
+        <TableHeaderColumn hidden dataSort dataField="_id" isKey>
+          Booking ID
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="agent.name">
+          Agent Name
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="pkg.name">
+          Package
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="tourist.name">
+          Tourist
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          width="100"
+          dataSort
+          dataField="tourist.adult"
+        >
+          Adult
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          dataAlign="right"
+          headerAlign="right"
+          width="100"
+          dataSort
+          dataField="tourist.child"
+        >
+          Child
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          width="100"
+          dataFormat={this.renderViewButton}
+          headerAlign="center"
+          dataAlign="center"
+          export={false}
+        >
+          Action
+        </TableHeaderColumn>
+      </BootstrapTable>
+    )
+  }
+}
+
+const mapStateToProps = ({
+  booking: { bookings, visibilityFilter: { status } }
+}) => {
+  return {
+    bookings: filter(
+      bookings,
+      booking =>
+        status === waiting
+          ? booking.status === waiting || booking.status === readed
+          : booking.status === status
+    ).map(flat)
+  }
+}
+
+export default connect(mapStateToProps, actions)(Table)
--- client/company-web/src/pages/manageBooking/bookingDataTable/index.js
+++ client/company-web/src/pages/manageBooking/bookingDataTable/index.js
@@ -1,36 +1,13 @@
-import _ from 'lodash'
 import React, { PureComponent } from 'react'
-import flat from 'flat'
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
-import { connect } from 'react-redux'
 
+import Table from './Table'
 import Card from '../../../components/Card'
 import ManageModal from './ManageModal'
 import FilterDate from './FilterDate'
 import FilterLinks from './FilterLinks'
-import * as actions from '../../../actions'
-import { waiting, readed } from '../../../actions/bookingStatus'
 
 class BookingDataTable extends PureComponent {
-  componentDidMount() {
-    this.props.fetchBookings(this.props.date)
-  }
-
-  renderViewButton = (cell, row) => {
-    const { openManageBookingModal } = this.props
-    return (
-      <button
-        className="btn btn-info btn-sm"
-        style={{ margin: 0 }}
-        onClick={() => openManageBookingModal(row._id, row.status)}
-      >
-        View
-      </button>
-    )
-  }
-
   render() {
-    const { bookings } = this.props
     return (
       <Card title="Bookings" description="Manage booking">
         <div className="row">
@@ -41,54 +18,7 @@ class BookingDataTable extends PureComponent {
             <FilterLinks />
           </div>
           <div className="col-md-12 col-sm-12">
-            <BootstrapTable
-              data={bookings}
-              height={340}
-              exportCSV
-              search
-              scrollTop="Bottom"
-              options={{ clearSearch: true }}
-            >
-              <TableHeaderColumn hidden dataSort dataField="_id" isKey>
-                Booking ID
-              </TableHeaderColumn>
-              <TableHeaderColumn dataSort dataField="agent.name">
-                Agent Name
-              </TableHeaderColumn>
-              <TableHeaderColumn dataSort dataField="pkg.name">
-                Package
-              </TableHeaderColumn>
-              <TableHeaderColumn dataSort dataField="tourist.name">
-                Tourist
-              </TableHeaderColumn>
-              <TableHeaderColumn
-                headerAlign="right"
-                dataAlign="right"
-                width="100"
-                dataSort
-                dataField="tourist.adult"
-              >
-                Adult
-              </TableHeaderColumn>
-              <TableHeaderColumn
-                dataAlign="right"
-                headerAlign="right"
-                width="100"
-                dataSort
-                dataField="tourist.child"
-              >
-                Child
-              </TableHeaderColumn>
-              <TableHeaderColumn
-                width="100"
-                dataFormat={this.renderViewButton}
-                headerAlign="center"
-                dataAlign="center"
-                export={false}
-              >
-                Action
-              </TableHeaderColumn>
-            </BootstrapTable>
+            <Table />
           </div>
         </div>
         <ManageModal />
@@ -97,19 +27,4 @@ class BookingDataTable extends PureComponent {
   }
 }
 
-const mapStateToProps = ({
-  booking: { bookings, visibilityFilter: { status, date } }
-}) => {
-  return {
-    bookings: _.filter(
-      bookings,
-      booking =>
-        status === waiting
-          ? booking.status === waiting || booking.status === readed
-          : booking.status === status
-    ).map(flat),
-    date
-  }
-}
-
-export default connect(mapStateToProps, actions)(BookingDataTable)
+export default BookingDataTable
