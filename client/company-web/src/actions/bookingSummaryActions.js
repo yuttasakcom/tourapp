1c9e6e3fddd07662e0827065d86e98e331c817e0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-02 20:28:14 +0700

    refactor

--- client/company-web/src/actions/bookingSummaryActions.js
+++ client/company-web/src/actions/bookingSummaryActions.js
@@ -3,12 +3,8 @@ import axios from './axios'
 import { FETCH_BOOKINGS_SUMMARY_SUCCESS } from './types'
 
 export const fetchBookingsSummary = date => async dispatch => {
-  let query = ''
-  if (date) {
-    query = `?date=${date}`
-  }
   try {
-    const { data } = await axios.get(`/bookingsSummary${query}`)
+    const { data } = await axios.get(`/bookingsSummary?date=${date}`)
     dispatch({ type: FETCH_BOOKINGS_SUMMARY_SUCCESS, payload: data })
   } catch (e) {
     console.error(e)
