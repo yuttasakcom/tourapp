218c496f0937837444166b23e326825cc54e5000
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-02 12:15:15 +0700

    use bootstrap table instend

--- client/agent-web/src/pages/manageBooking/bookingDataTable/index.js
+++ client/agent-web/src/pages/manageBooking/bookingDataTable/index.js
@@ -1,68 +1,13 @@
-import _ from 'lodash'
 import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
 
 import FilterDate from './FilterDate'
 import Card from '../../../components/Card'
-import DataTable from '../../../components/dataTable'
+import Table from './Table'
 import ManageModal from './ManageModal'
 import FilterLinks from './FilterLinks'
-import * as actions from '../../../actions'
 
 class BookingDataTable extends PureComponent {
-  componentDidMount() {
-    this.props.fetchBookings(this.props.date)
-  }
-
-  renderTableBody = () => {
-    const { bookings, openManageBookingModal } = this.props
-
-    if (!bookings) {
-      return null
-    }
-
-    return _.map(bookings, booking =>
-      <tr key={booking._id}>
-        <td>
-          {booking.company.name}
-        </td>
-        <td>
-          {booking.company.email}
-        </td>
-        <td>
-          {booking.pkg.name}
-        </td>
-        <td>
-          {booking.tourist.name}
-        </td>
-        <td>
-          {booking.tourist.adult}
-        </td>
-        <td>
-          {booking.tourist.child}
-        </td>
-        <td style={{ textAlign: 'center' }}>
-          <button
-            className="btn btn-info btn-sm"
-            style={{ margin: 0 }}
-            onClick={() => openManageBookingModal(booking._id)}
-          >
-            View
-          </button>
-        </td>
-      </tr>
-    )
-  }
-
   render() {
-    const tableTitles = [
-      'Company Name',
-      'Company Email',
-      'Package',
-      'Tourist',
-      'Adult',
-      'Child'
-    ]
     return (
       <Card title="Bookings" description="Manage booking">
         <div className="row">
@@ -72,14 +17,8 @@ class BookingDataTable extends PureComponent {
           <div className="col-md-7">
             <FilterLinks />
           </div>
-          <div
-            className="col-md-12 col-sm-12"
-            style={{ display: 'block', overflow: 'auto', height: 400 }}
-          >
-            <DataTable
-              tableTitles={tableTitles}
-              renderTableBody={this.renderTableBody}
-            />
+          <div className="col-md-12 col-sm-12">
+            <Table />
           </div>
           <ManageModal />
         </div>
@@ -88,11 +27,4 @@ class BookingDataTable extends PureComponent {
   }
 }
 
-const mapStateToProps = ({
-  manageBooking: { bookings, visibilityFilter: { status, date } }
-}) => ({
-  bookings: _.filter(bookings, booking => booking.status === status),
-  date
-})
-
-export default connect(mapStateToProps, actions)(BookingDataTable)
+export default BookingDataTable
