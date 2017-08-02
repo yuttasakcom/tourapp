67607a591cdb11444611e084f5df8339d2b5ace6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-02 16:18:13 +0700

    add Card

--- client/company-web/src/pages/bookingSummary/index.js
+++ client/company-web/src/pages/bookingSummary/index.js
@@ -1,12 +1,15 @@
 import React, { PureComponent } from 'react'
 
+import Card from '../../components/Card'
 import bookingSummary from '../../resources/images/booking_summary.png'
 
 class BookingSummary extends PureComponent {
   render() {
     return (
-      <div>
-        <img src={bookingSummary} alt="booking summary" />
+      <div className="container-fluid">
+        <Card title="Booking Summary" description="Show Booking Summary">
+          <img src={bookingSummary} alt="booking summary" />
+        </Card>
       </div>
     )
   }
