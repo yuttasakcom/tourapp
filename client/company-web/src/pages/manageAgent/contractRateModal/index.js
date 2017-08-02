14f3c9bfbafffe9b36ffcb85b8a4e0f247803e51
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-02 11:14:28 +0700

    refactor

--- client/company-web/src/pages/manageAgent/contractRateModal/index.js
+++ client/company-web/src/pages/manageAgent/contractRateModal/index.js
@@ -1,49 +1,13 @@
-import _ from 'lodash'
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Modal, Button } from 'react-bootstrap'
 
-import DataTable from '../../../components/dataTable'
+import Table from './Table'
 import OfferSpecialPriceModal from './OfferSpecialPriceModal'
 import ResetPriceModal from './ResetPriceModal'
 import * as actions from '../../../actions'
 
 class ContractRateModal extends PureComponent {
-  renderTableBody = () => {
-    const {
-      contractRates,
-      openOfferSpecialPriceModal,
-      openResetPriceModal
-    } = this.props
-
-    if (!contractRates) {
-      return null
-    }
-
-    return _.map(contractRates, contractRate =>
-      <tr key={contractRate._id}>
-        <td>{contractRate._id}</td>
-        <td>{contractRate.name}</td>
-        <td>{contractRate.priceAdult.toLocaleString()}</td>
-        <td>{contractRate.priceChild.toLocaleString()}</td>
-        <td style={{ textAlign: 'center' }}>
-          <button
-            className="btn btn-info btn-sm"
-            onClick={() => openOfferSpecialPriceModal(contractRate._id)}
-          >
-            Offer Special Price
-          </button>
-          <button
-            className="btn btn-danger btn-sm"
-            onClick={() => openResetPriceModal(contractRate._id)}
-          >
-            Reset
-          </button>
-        </td>
-      </tr>
-    )
-  }
-
   render() {
     const { showModal, closeContractRateModal, agent } = this.props
 
@@ -51,18 +15,15 @@ class ContractRateModal extends PureComponent {
       return null
     }
 
-    const tableTitles = ['_id', 'Name', 'Adult Price', 'Child Price']
-
     return (
       <Modal show={showModal} onHide={closeContractRateModal} bsSize="lg">
         <Modal.Header closeButton>
-          <Modal.Title>Agent {agent.email} Contract Rate</Modal.Title>
+          <Modal.Title>
+            Agent {agent.email} Contract Rate
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <DataTable
-            tableTitles={tableTitles}
-            renderTableBody={this.renderTableBody}
-          />
+          <Table />
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={closeContractRateModal}>Close</Button>
@@ -77,8 +38,7 @@ class ContractRateModal extends PureComponent {
 const mapStateToProps = ({ agent }) => {
   return {
     showModal: agent.showContractRateModal,
-    agent: agent.agents[agent.selectedAgent],
-    contractRates: agent.selectedAgentContractRates
+    agent: agent.agents[agent.selectedAgent]
   }
 }
 
