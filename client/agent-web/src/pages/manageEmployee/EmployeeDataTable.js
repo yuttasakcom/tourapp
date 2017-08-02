d471e1562df61d9fe8450184f0363559d65e9f21
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-02 11:34:25 +0700

    use bootstrap table instend

--- client/agent-web/src/pages/manageEmployee/EmployeeDataTable.js
+++ client/agent-web/src/pages/manageEmployee/EmployeeDataTable.js
@@ -1,61 +1,15 @@
-import _ from 'lodash'
 import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
 
 import Card from '../../components/Card'
-import DataTable from '../../components/dataTable'
+import Table from './Table'
 import DeleteModal from './DeleteModal'
 import EditModal from './EditModal'
-import * as actions from '../../actions'
 
 class EmployeeDataTable extends PureComponent {
-  componentDidMount() {
-    this.props.fetchEmployees()
-  }
-
-  renderTableBody = () => {
-    const {
-      employees,
-      openDeleteEmployeeModal,
-      openEditEmployeeModal
-    } = this.props
-
-    if (!employees) {
-      return null
-    }
-
-    return _.map(employees, employee =>
-      <tr key={employee._id}>
-        <td>{employee.name}</td>
-        <td>{employee.phoneNumber}</td>
-        <td>{employee.email}</td>
-        <td>{employee.password}</td>
-        <td style={{ textAlign: 'center' }}>
-          <button
-            className="btn btn-warning btn-sm"
-            onClick={() => openEditEmployeeModal(employee._id)}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-danger btn-sm"
-            onClick={() => openDeleteEmployeeModal(employee._id)}
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    )
-  }
-
   render() {
-    const tableTitles = ['Name', 'Phone Number', 'Email', 'Password']
     return (
       <Card title="Employees" description="Manage Employees">
-        <DataTable
-          tableTitles={tableTitles}
-          renderTableBody={this.renderTableBody}
-        />
+        <Table />
         <EditModal />
         <DeleteModal />
       </Card>
@@ -63,8 +17,4 @@ class EmployeeDataTable extends PureComponent {
   }
 }
 
-const mapStateToProps = state => ({
-  employees: state.employee.employees
-})
-
-export default connect(mapStateToProps, actions)(EmployeeDataTable)
+export default EmployeeDataTable
---
+++ client/agent-web/src/pages/manageEmployee/Table.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { connect } from 'react-redux'
+
+import * as actions from '../../actions'
+
+class Table extends PureComponent {
+  componentDidMount() {
+    this.props.fetchEmployees()
+  }
+
+  renderAction = (cell, row) => {
+    const { openDeleteEmployeeModal, openEditEmployeeModal } = this.props
+    return (
+      <div>
+        <button
+          className="btn btn-warning btn-sm"
+          style={{ margin: 0 }}
+          onClick={() => openEditEmployeeModal(row._id)}
+        >
+          Edit
+        </button>
+        <button
+          className="btn btn-danger btn-sm"
+          style={{ margin: 0 }}
+          onClick={() => openDeleteEmployeeModal(row._id)}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  }
+
+  render() {
+    const { employees } = this.props
+    return (
+      <BootstrapTable
+        data={Object.values(employees)}
+        exportCSV
+        search
+        striped
+        hover
+        condensed
+        options={{ clearSearch: true }}
+      >
+        <TableHeaderColumn hidden dataField="_id" isKey>
+          Employee ID
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="name">
+          Name
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="phoneNumber">
+          Phone Number
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="email">
+          Email
+        </TableHeaderColumn>
+        <TableHeaderColumn dataSort dataField="password">
+          Password
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          width="180"
+          dataFormat={this.renderAction}
+          headerAlign="center"
+          dataAlign="center"
+          export={false}
+        >
+          Action
+        </TableHeaderColumn>
+      </BootstrapTable>
+    )
+  }
+}
+
+const mapStateToProps = ({ employee: { employees } }) => ({ employees })
+
+export default connect(mapStateToProps, actions)(Table)
