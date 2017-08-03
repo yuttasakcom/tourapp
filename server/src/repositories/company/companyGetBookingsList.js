eafccbbea611c5565cd9582ffdba63471ae4e71d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-03 08:38:53 +0700

    fix for consistency

--- server/src/repositories/company/companyGetBookingsList.js
+++ server/src/repositories/company/companyGetBookingsList.js
@@ -2,8 +2,8 @@ const moment = require('moment')
 const Booking = require('../../models/booking')
 
 module.exports = (companyId, date) => {
-  const gteDate = moment(parseInt(date, 10))
-  const ltDate = moment(gteDate).add(1, 'days')
+  const gteDate = moment(parseInt(date, 10)).toDate()
+  const ltDate = moment(gteDate).add(1, 'days').toDate()
   return Booking.find({
     company: companyId,
     'tourist.date': { $gte: gteDate, $lt: ltDate }
