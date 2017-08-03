1a9179f2ea15757469f3b9f632fbce6ec8715641
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-03 10:19:26 +0700

    add booking summary api

--- server/src/repositories/company/companyGetBookingsSummary.js
+++ server/src/repositories/company/companyGetBookingsSummary.js
@@ -1,21 +1,37 @@
+const ObjectId = require('mongoose').Types.ObjectId
 const moment = require('moment')
 const Booking = require('../../models/booking')
 
 module.exports = (companyId, date) => {
-  const gteDate = moment(parseInt(date, 10))
-  const ltDate = moment(gteDate).add(1, 'days')
+  const gteDate = moment(parseInt(date, 10)).toDate()
+  const ltDate = moment(gteDate).add(1, 'days').toDate()
   return Booking.aggregate([
     {
       $match: {
-        company: companyId,
-        'tourist.date': { $gte: gteDate, $lt: ltDate }
+        $and: [
+          { company: ObjectId(companyId) },
+          { 'tourist.date': { $gte: gteDate, $lt: ltDate } }
+        ]
       }
     },
     {
       $group: {
-        _id: '$_id',
-        total: { $sum: '$pkg.adult' }
+        _id: '$pkg.name',
+        totalBooking: { $sum: 1 },
+        totalAdultIncome: { $sum: '$pkg.priceAdult' },
+        totalChildIncome: { $sum: '$pkg.priceChild' },
+        totalAdult: { $sum: '$tourist.adult' },
+        totalChild: { $sum: '$tourist.child' }
       }
+    },
+    {
+      $addFields: {
+        total: { $add: ['$totalAdult', '$totalChild'] },
+        totalIncome: { $add: ['$totalAdultIncome', '$totalChildIncome'] }
+      }
+    },
+    {
+      $sort: { total: -1 }
     }
   ])
 }
