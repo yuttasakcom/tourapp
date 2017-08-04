777c4a757ad06a4aa762a32a05bb234fb53ae9a0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-04 10:24:24 +0700

    remove change-case and use lodash instend

--- client/agent-web/src/components/layout/toolbar/leftZone/Title.js
+++ client/agent-web/src/components/layout/toolbar/leftZone/Title.js
@@ -1,15 +1,19 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import { titleCase } from 'change-case'
+import { startCase } from 'lodash'
 
 class Title extends PureComponent {
   render() {
-    return <a className="navbar-brand">{this.props.title}</a>
+    return (
+      <a className="navbar-brand">
+        {this.props.title}
+      </a>
+    )
   }
 }
 
 const mapStateToProps = state => {
-  return { title: titleCase(state.router.location.pathname) }
+  return { title: startCase(state.router.location.pathname) }
 }
 
 export default connect(mapStateToProps)(Title)
