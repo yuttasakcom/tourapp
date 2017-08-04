6835f70e6ee67b6201874ea28f0c37a5efe10fef
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-04 22:40:59 +0700

    remove date state

--- client/company-web/src/reducers/dashboardReducer.js
+++ client/company-web/src/reducers/dashboardReducer.js
@@ -1,13 +1,7 @@
-import moment from 'moment'
-
 import { FETCH_BOOKINGS_DASHBOARD_SUCCESS } from '../actions/types'
 
-const dateNowStart = moment().startOf('day')
-const dateNowEnd = dateNowStart.clone().endOf('day')
-
 const initialState = {
-  bookings: [],
-  visibilityFilter: { dateStart: dateNowStart, dateNowEnd: dateNowEnd }
+  bookings: []
 }
 
 export default (state = initialState, action) => {
@@ -15,11 +9,7 @@ export default (state = initialState, action) => {
     case FETCH_BOOKINGS_DASHBOARD_SUCCESS:
       return {
         ...state,
-        bookings: action.payload.data,
-        visibilityFilter: {
-          dateStart: action.payload.dateStart,
-          dateEnd: action.payload.dateEnd
-        }
+        bookings: action.payload
       }
 
     default:
