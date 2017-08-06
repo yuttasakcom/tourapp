907467e60528bdd613148d6d6c2799cef575e645
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-06 10:53:12 +0700

    initail pdf with thai font

--- server/src/reports/company/bookingsSummary.js
+++ server/src/reports/company/bookingsSummary.js
@@ -1,5 +1,14 @@
+const PDFDocument = require('pdfkit')
+const path = require('path')
+
 const repo = require('../../repositories')
 
 module.exports = async (req, res, next) => {
-  return res.send({ message: 'aha' })
+  const doc = new PDFDocument()
+  doc
+    .font(path.join(__dirname, '../fonts/THSarabunNew/THSarabunNew.ttf'))
+    .fontSize(25)
+    .text('ภาษาไทย', 100, 100)
+  doc.pipe(res)
+  doc.end()
 }
