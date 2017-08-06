1abb5b351392af69e4528eefeb29439b4ce2402f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-06 10:28:54 +0700

    add print button

--- client/company-web/src/pages/bookingSummary/index.js
+++ client/company-web/src/pages/bookingSummary/index.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react'
 import Table from './Table'
 import Card from '../../components/Card'
 import FilterDate from './FilterDate'
+import { openReport } from '../../helpers'
 
 class BookingSummary extends PureComponent {
   render() {
@@ -10,6 +11,14 @@ class BookingSummary extends PureComponent {
       <div className="container-fluid">
         <Card title="Booking Summary" description="Show Booking Summary">
           <div className="row">
+            <div className="col-md-12">
+              <button
+                className="btn btn-primary pull-right"
+                onClick={async () => openReport('bookingsSummary')}
+              >
+                Print
+              </button>
+            </div>
             <div className="col-md-12">
               <FilterDate />
             </div>
