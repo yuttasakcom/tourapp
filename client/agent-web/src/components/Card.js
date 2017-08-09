95e92bc6252aa4b6d0fc94e7b2ac86c68ad2773b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-09 08:43:47 +0700

    fix css

--- client/agent-web/src/components/Card.js
+++ client/agent-web/src/components/Card.js
@@ -2,18 +2,15 @@ import React, { PureComponent } from 'react'
 
 class Card extends PureComponent {
   render() {
-    const { title, description, children } = this.props
+    const { title, children, style } = this.props
     return (
       <div className="card">
         <div className="card-header" data-background-color="purple">
-          <h4 className="title">
+          <h4 className="title" style={{ margin: 0 }}>
             {title}
           </h4>
-          <p className="category">
-            {description}
-          </p>
         </div>
-        <div className="card-content table-responsive">
+        <div className="card-content table-responsive" style={style}>
           {children}
         </div>
       </div>
