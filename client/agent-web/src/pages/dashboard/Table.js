1ce131db7fb56756504c47bbea1878183c2c13cd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-09 17:41:56 +0700

    add dashboard table

---
+++ client/agent-web/src/pages/dashboard/Table.js
@@ -0,0 +1,69 @@
+import React, { PureComponent } from 'react'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { connect } from 'react-redux'
+
+import * as actions from '../../actions'
+
+class Table extends PureComponent {
+  componentDidMount() {
+    this.props.fetchDashboard()
+  }
+
+  render() {
+    const { bookingsSummary } = this.props
+    return (
+      <BootstrapTable
+        data={bookingsSummary}
+        exportCSV
+        height={400}
+        search
+        striped
+        hover
+        condensed
+        options={{ clearSearch: true }}
+      >
+        <TableHeaderColumn dataSort dataField="_id" isKey>
+          Package Name
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          dataSort
+          dataField="totalBooking"
+        >
+          Booking Total
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          dataSort
+          dataField="totalAdult"
+        >
+          Adult
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          dataSort
+          dataField="totalChild"
+        >
+          Child
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          dataSort
+          dataField="totalSeat"
+        >
+          Total Seat
+        </TableHeaderColumn>
+      </BootstrapTable>
+    )
+  }
+}
+
+const mapStateToProps = ({ dashboard: { bookingsSummary } }) => ({
+  bookingsSummary
+})
+
+export default connect(mapStateToProps, actions)(Table)
