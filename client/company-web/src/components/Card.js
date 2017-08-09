933971b1f624c3628580827d6d0326625b06ed2d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-09 08:41:47 +0700

    remove card description

--- client/company-web/src/components/Card.js
+++ client/company-web/src/components/Card.js
@@ -2,16 +2,13 @@ import React, { PureComponent } from 'react'
 
 class Card extends PureComponent {
   render() {
-    const { title, description, children, style } = this.props
+    const { title, children, style } = this.props
     return (
       <div className="card">
         <div className="card-header" data-background-color="purple">
           <h4 className="title">
             {title}
           </h4>
-          <p className="category">
-            {description}
-          </p>
         </div>
         <div className="card-content table-responsive" style={style}>
           {children}
