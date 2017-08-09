2ed607335e291b8305af65060c33bf0fe6a007e3
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-09 07:09:14 +0700

    render table

--- client/company-web/src/pages/dashboard/index.js
+++ client/company-web/src/pages/dashboard/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 
+import Table from './Table'
 import Card from '../../components/Card'
 import * as actions from '../../actions'
 
@@ -9,15 +10,32 @@ class Dashboard extends PureComponent {
     this.props.fetchDashboard()
   }
 
+  renderDashboard() {
+    const { bookingsSummaries } = this.props
+    return bookingsSummaries.map(bookingsSummary => {
+      return (
+        <div className="col-md-4">
+          <Table bookingsSummary={bookingsSummary} />
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <Card title="Dashboard" description="Show Dashboard">
-          <div className="row" />
+          <div className="row">
+            {this.renderDashboard()}
+          </div>
         </Card>
       </div>
     )
   }
 }
 
-export default connect(null, actions)(Dashboard)
+const mapStateToProps = ({ dashboard: { bookingsSummaries } }) => ({
+  bookingsSummaries
+})
+
+export default connect(mapStateToProps, actions)(Dashboard)
