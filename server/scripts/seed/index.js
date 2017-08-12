dc5cde370708cc372cb43ac0024821d7cacea33b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-12 10:30:30 +0700

    wireup hotel seed data

--- server/scripts/seed/index.js
+++ server/scripts/seed/index.js
@@ -4,9 +4,11 @@ const Pkg = require('../../src/models/pkg')
 const Company = require('../../src/models/company')
 const Agent = require('../../src/models/agent')
 const Booking = require('../../src/models/booking')
+const Hotel = require('../../src/models/hotel')
 const { MONGO_DB_HOST } = require('../../src/config')
 const { companies, agents, pkgs } = require('./masterData')
 const bookings = require('./bookingsData')
+const hotels = require('./hotelsData')
 
 mongoose.Promise = global.Promise
 
@@ -17,7 +19,8 @@ mongoose.connection.once('open', async () => {
     Company.insertMany(companies),
     Agent.insertMany(agents),
     Pkg.insertMany(pkgs),
-    Booking.insertMany(bookings)
+    Booking.insertMany(bookings),
+    Hotel.insertMany(hotels)
   ])
   mongoose.connection.close()
   console.log('Seed data completed')
