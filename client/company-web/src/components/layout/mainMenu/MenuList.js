a64d710bd1453e4860fe653266f977fe7ac08b2d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-14 20:35:09 +0700

    update icons

--- client/company-web/src/components/layout/mainMenu/MenuList.js
+++ client/company-web/src/components/layout/mainMenu/MenuList.js
@@ -8,8 +8,8 @@ class MenuList extends PureComponent {
       <ul className="nav">
         <MenuItem icon="dashboard" path="/dashboard" />
         <MenuItem icon="assignment" path="/manage-booking" />
-        <MenuItem icon="assignment" path="/booking-summary" />
-        <MenuItem icon="assignment" path="/manage-bus-path" />
+        <MenuItem icon="chrome_reader_mode" path="/booking-summary" />
+        <MenuItem icon="directions_bus" path="/manage-bus-path" />
         <MenuItem icon="widgets" path="/manage-tour-package" />
         <MenuItem icon="contact_phone" path="/manage-agent" />
       </ul>
