474d6b40bab3cf97ca53aa4eb76cf8d0d8d5cff7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-14 13:42:01 +0700

    shape object in server

--- server/src/controllers/company/getBookingsHotelsSummary.js
+++ server/src/controllers/company/getBookingsHotelsSummary.js
@@ -7,5 +7,11 @@ module.exports = async (req, res, next) => {
     req.query.dateStart,
     req.query.dateEnd
   )
-  return res.send(bookingsHotelsSummary)
+  return res.send(
+    bookingsHotelsSummary.map(hotel => ({
+      _id: hotel._id._id,
+      name: hotel._id.name,
+      total: hotel.total
+    }))
+  )
 }
