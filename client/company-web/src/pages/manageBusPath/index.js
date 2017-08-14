c8002b7362eb9c46f5c9e06fd5a8b8775e1cf686
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-14 10:03:27 +0700

    initial manage bus path route

---
+++ client/company-web/src/pages/manageBusPath/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+import Card from '../../components/Card'
+
+class ManageBusPath extends PureComponent {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Card title="Bus Path" />
+      </div>
+    )
+  }
+}
+
+export default ManageBusPath
