ed1303ec62929c3e1417042bbc53167567b39c2b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-14 14:33:35 +0700

    when fetch booking must alocate 8 options

--- client/company-web/src/reducers/busPathReducer.js
+++ client/company-web/src/reducers/busPathReducer.js
@@ -1,19 +1,25 @@
 import mapKeys from 'lodash/mapKeys'
+import times from 'lodash/times'
 import moment from 'moment'
 
 import { FETCH_BOOKINGS_HOTELS_SUMMARY_SUCCESS } from '../actions/types'
 
 const initialState = {
-  hotels: {},
+  hotelsOptions: [],
   visibilityFilter: { date: moment().startOf('day') }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_BOOKINGS_HOTELS_SUMMARY_SUCCESS:
+      const hotelsOption = mapKeys(action.payload.data, '_id')
+      const hotelsOptions = []
+      times(8, () => {
+        hotelsOptions.push(hotelsOption)
+      })
       return {
         ...state,
-        hotels: mapKeys(action.payload.data, '_id'),
+        hotelsOptions,
         visibilityFilter: { date: action.payload.date }
       }
 
