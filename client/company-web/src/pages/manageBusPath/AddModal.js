ee84bb496b2d815b1815a9bb88a10b56ee71b77f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-15 10:37:19 +0700

    add buspath AddModal component

---
+++ client/company-web/src/pages/manageBusPath/AddModal.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import Modal from 'react-bootstrap/lib/Modal'
+
+import BusPathForm from './BusPathForm'
+import * as actions from '../../actions'
+
+class AddModal extends PureComponent {
+  onSubmit = values => {
+    this.props.addBusPath(values)
+  }
+
+  render() {
+    const { showModal, closeAddBusPathModal } = this.props
+    return (
+      <Modal show={showModal} onHide={closeAddBusPathModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Bus Path</Modal.Title>
+        </Modal.Header>
+        <BusPathForm
+          onSubmit={this.onSubmit}
+          closeModal={closeAddBusPathModal}
+        />
+      </Modal>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  showModal: state.busPath.showAddBusPathModal
+})
+
+export default connect(mapStateToProps, actions)(AddModal)
