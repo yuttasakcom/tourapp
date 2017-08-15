7a99a4ce8c7f93f55b1dc99da47d8f3193d9f59a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-15 16:34:42 +0700

    add hotels field

--- client/company-web/src/pages/manageBusPath/BusPathForm.js
+++ client/company-web/src/pages/manageBusPath/BusPathForm.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import Button from 'react-bootstrap/lib/Button'
 import Modal from 'react-bootstrap/lib/Modal'
@@ -7,7 +8,14 @@ import renderField from '../../components/renderField'
 
 class BusPathForm extends PureComponent {
   render() {
-    const { handleSubmit, reset, pristine, submitting, closeModal } = this.props
+    const {
+      handleSubmit,
+      reset,
+      pristine,
+      submitting,
+      closeModal,
+      hotels
+    } = this.props
 
     return (
       <form onSubmit={handleSubmit}>
@@ -27,6 +35,16 @@ class BusPathForm extends PureComponent {
                 component={renderField}
                 label="Description"
                 type="text"
+              />
+            </div>
+            <div className="col-md-12">
+              <Field
+                muti
+                name="hotels"
+                component={renderField}
+                options={hotels}
+                label="Hotel"
+                type="select"
               />
             </div>
           </div>
@@ -57,4 +75,13 @@ const validate = values => {
   return errors
 }
 
-export default reduxForm({ form: 'busPath', validate })(BusPathForm)
+const mapStateToProps = ({ busPath: { hotels } }) => ({
+  hotels: hotels.map(hotel => ({
+    value: hotel._id,
+    label: hotel.name
+  }))
+})
+
+export default connect(mapStateToProps)(
+  reduxForm({ form: 'busPath', validate })(BusPathForm)
+)
