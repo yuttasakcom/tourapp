f790217c856062ccc9e421bf3a01d6baa2b8e1d2
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-15 10:44:03 +0700

    set initial state

--- client/company-web/src/reducers/busPathReducer.js
+++ client/company-web/src/reducers/busPathReducer.js
@@ -1,4 +1,10 @@
-const initialState = {}
+const initialState = {
+  busPaths: {},
+  selectedBusPath: null,
+  showAddBusPathModal: false,
+  showEditBusPathModal: false,
+  showDeleteBusPathModal: false
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
