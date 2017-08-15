34e7fb3f3a79494980eea0f3627c9b1c9f3101b4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-15 10:13:28 +0700

    wireup manageBusPath component

--- client/company-web/src/pages/manageBusPath/index.js
+++ client/company-web/src/pages/manageBusPath/index.js
@@ -1,49 +1,36 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import sumBy from 'lodash/sumBy'
 
-import FilterDate from './FilterDate'
-import BusPathList from './BusPathList'
 import Card from '../../components/Card'
+import BusPathDataTable from './BusPathDataTable'
+import AddModal from './AddModal'
+import * as actions from '../../actions'
 
 class ManageBusPath extends PureComponent {
   render() {
-    const { totalHotels, totalSeats } = this.props
-
     return (
       <div className="container-fluid">
-        <Card title="Bus Path" style={{ height: '800px' }}>
+        <Card title="Bus Paths">
           <div className="row">
-            <div className="col-md-4 col-sm-4">
-              <FilterDate />
-            </div>
-            <div style={{ marginTop: 20 }} className="col-md-4 col-sm-4">
-              <a style={{ marginRight: 10 }}>
-                จำนวนโรงแรม <span className="badge">{totalHotels}</span>
-              </a>
-              <a>
-                จำนวนผู้โดยสาร <span className="badge">{totalSeats}</span>
-              </a>
-            </div>
-            <div className="col-md-4 col-sm-4">
-              <button className="btn btn-primary pull-right" onClick={() => ''}>
-                Print
-              </button>
-              <button className="btn btn-success pull-right" onClick={() => ''}>
-                Update
+            <div className="col-md-12">
+              <button
+                className="btn btn-primary pull-right"
+                onClick={this.props.openAddPkgModal}
+              >
+                Add
               </button>
             </div>
           </div>
-          <BusPathList />
+          <div className="row">
+            <div className="col-md-12">
+              <BusPathDataTable />
+            </div>
+          </div>
         </Card>
+        <AddModal />
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ busPath: { hotels } }) => ({
-  totalHotels: hotels.length,
-  totalSeats: sumBy(hotels, 'total')
-})
-
-export default connect(mapStateToProps)(ManageBusPath)
+export default connect(null, actions)(ManageBusPath)
