5c4959395b01f20eecabc63b3b26652bd1eb3145
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-16 16:43:18 +0700

    update query to find when update

--- server/src/repositories/company/companyAddBusPath.js
+++ server/src/repositories/company/companyAddBusPath.js
@@ -1,11 +1,15 @@
 const Company = require('../../models/company')
 
 module.exports = (companyId, busPathProps) =>
-  Company.update(
+  Company.findOneAndUpdate(
     { _id: companyId, 'busPaths.name': { $ne: busPathProps.name } },
     {
       $push: {
         busPaths: busPathProps
       }
+    },
+    {
+      projection: { busPaths: { $elemMatch: { name: busPathProps.name } } },
+      new: true
     }
   )
