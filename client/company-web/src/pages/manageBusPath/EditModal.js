c3cf4629cf69fcdc8476c210cb2c5d8e2b80c81a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-16 17:29:43 +0700

    prepare data before edit

--- client/company-web/src/pages/manageBusPath/EditModal.js
+++ client/company-web/src/pages/manageBusPath/EditModal.js
@@ -1,12 +1,14 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import Modal from 'react-bootstrap/lib/Modal'
+import map from 'lodash/map'
 
 import BusPathForm from './BusPathForm'
 import * as actions from '../../actions'
 
 class EditModal extends PureComponent {
   onSubmit = values => {
+    values.hotels = map(values.hotels, 'value')
     this.props.editBusPath(this.props.busPath, values)
   }
 
