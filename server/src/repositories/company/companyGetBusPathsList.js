5a3d5a2b10aaad0467432d74e30d99fefb8f1ae5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-16 18:06:05 +0700

    populate busPaths hotels for display

--- server/src/repositories/company/companyGetBusPathsList.js
+++ server/src/repositories/company/companyGetBusPathsList.js
@@ -1,6 +1,8 @@
 const Company = require('../../models/company')
 
 module.exports = async companyId => {
-  const { busPaths } = await Company.findById(companyId, { busPaths: 1 })
+  const { busPaths } = await Company.findById(companyId, {
+    busPaths: 1
+  }).populate('busPaths.hotels')
   return busPaths
 }
