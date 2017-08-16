8d4272ab2f529fc750d44978f511bbc394ca1b9d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-16 16:34:33 +0700

    handle error when duplicate and send only updated busPath to client

--- server/src/controllers/company/addBusPath.js
+++ server/src/controllers/company/addBusPath.js
@@ -4,5 +4,10 @@ module.exports = async (req, res, next) => {
   const companyId = req.user._id
   const busPathProps = req.body
   const company = await repo.companyAddBusPath(companyId, busPathProps)
-  return res.status(201).send(company)
+  if (company) {
+    return res.status(201).send(company.busPaths[0])
+  }
+  const err = new Error('sorry bus path name can not dupplicate')
+  err.status = 422
+  return next(err)
 }
