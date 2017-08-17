a115182967220766da795e324755d4b7d42c9303
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-17 17:02:12 +0700

    show rest hotels and seats

--- client/company-web/src/pages/printBusPath/Information.js
+++ client/company-web/src/pages/printBusPath/Information.js
@@ -1,26 +1,61 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import sumBy from 'lodash/sumBy'
+import intersectionBy from 'lodash/fp/intersectionBy'
+import flow from 'lodash/fp/flow'
+import map from 'lodash/fp/map'
+import flatten from 'lodash/fp/flatten'
 
 class Information extends PureComponent {
   render() {
-    const { totalHotels, totalSeats } = this.props
+    const {
+      totalHotels,
+      totalSeats,
+      selectedHotels,
+      selectedSeats
+    } = this.props
     return (
-      <div style={{ marginTop: 20 }} className="col-md-4 col-sm-4">
-        <a style={{ marginRight: 10 }}>
-          จำนวนโรงแรม <span className="badge">{totalHotels}</span>
-        </a>
-        <a>
-          จำนวนผู้โดยสาร <span className="badge">{totalSeats}</span>
-        </a>
+      <div style={{ marginTop: 10 }} className="col-md-4 col-sm-4">
+        <div className="row">
+          <a style={{ marginRight: 10 }}>
+            โรงแรม: ทั้งหมด <span className="badge">{totalHotels}</span>
+          </a>
+          <a>
+            คงเหลือ{' '}
+            <span className="badge">{totalHotels - selectedHotels}</span>
+          </a>
+        </div>
+        <div className="row">
+          <a style={{ marginRight: 10 }}>
+            ผู้โดยสาร: ทั้งหมด <span className="badge">{totalSeats}</span>
+          </a>
+          <a>
+            คงเหลือ <span className="badge">{totalSeats - selectedSeats}</span>
+          </a>
+        </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ printBusPath: { bookingsHotelsSummary } }) => ({
-  totalHotels: bookingsHotelsSummary.length,
-  totalSeats: sumBy(bookingsHotelsSummary, 'total')
-})
+const mapStateToProps = ({
+  printBusPath: { bookingsHotelsSummary, hotelsSelects }
+}) => {
+  const selectedHotels = flow(
+    map('values'),
+    flatten,
+    map(value => {
+      value._id = value.value
+      return value
+    }),
+    intersectionBy('_id', bookingsHotelsSummary)
+  )(hotelsSelects)
+  return {
+    totalHotels: bookingsHotelsSummary.length,
+    totalSeats: sumBy(bookingsHotelsSummary, 'total'),
+    selectedHotels: selectedHotels.length,
+    selectedSeats: sumBy(selectedHotels, 'total')
+  }
+}
 
 export default connect(mapStateToProps)(Information)
