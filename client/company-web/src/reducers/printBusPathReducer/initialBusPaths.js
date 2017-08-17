3e362d0d65f4e9bdfd9b327784c799f96d47ef21
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-17 15:01:03 +0700

    complete initial select value

--- client/company-web/src/reducers/printBusPathReducer/initialBusPaths.js
+++ client/company-web/src/reducers/printBusPathReducer/initialBusPaths.js
@@ -1,19 +1,36 @@
 import mapKeys from 'lodash/mapKeys'
 import intersectionBy from 'lodash/intersectionBy'
 import map from 'lodash/map'
+import flatten from 'lodash/flatten'
+import omit from 'lodash/omit'
+import difference from 'lodash/difference'
 
 export default (state, action) => {
   const { bookingsHotelsSummary, busPaths, date } = action.payload
   const options = mapKeys(bookingsHotelsSummary, '_id')
-  const hotelsSelects = map(busPaths, (busPath, index) => ({
-    options,
-    values: map(
-      intersectionBy(bookingsHotelsSummary, busPath.hotels, '_id'),
-      hotel => ({ value: hotel._id, label: `${hotel.name} (${hotel.total})` })
-    ),
-    busPathId: busPath._id,
-    busPathName: busPath.name
-  }))
+  const busPathsHotelIds = flatten(
+    map(busPaths, busPath => map(busPath.hotels, '_id'))
+  )
+  const hotelsSelects = map(busPaths, (busPath, index) => {
+    const initialValues = intersectionBy(
+      bookingsHotelsSummary,
+      busPath.hotels,
+      '_id'
+    )
+    const initialValuesHotelIds = map(initialValues, '_id')
+    return {
+      options: omit(
+        options,
+        difference(busPathsHotelIds, initialValuesHotelIds)
+      ),
+      values: map(initialValues, hotel => ({
+        value: hotel._id,
+        label: `${hotel.name} (${hotel.total})`
+      })),
+      busPathId: busPath._id,
+      busPathName: busPath.name
+    }
+  })
   return {
     ...state,
     bookingsHotelsSummary,
