a4e91da009115ac5f8dfddfbd671209547160d93
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-17 14:08:41 +0700

    initial value

--- client/company-web/src/reducers/printBusPathReducer/initialBusPaths.js
+++ client/company-web/src/reducers/printBusPathReducer/initialBusPaths.js
@@ -5,17 +5,15 @@ import map from 'lodash/map'
 export default (state, action) => {
   const { bookingsHotelsSummary, busPaths, date } = action.payload
   const options = mapKeys(bookingsHotelsSummary, '_id')
-  const myvalues = [...bookingsHotelsSummary, busPaths]
-  console.log(myvalues)
-  const hotelsSelects = []
-  map(busPaths, (busPath, index) => {
-    hotelsSelects.push({
-      options,
-      values: null,
-      busPathId: busPath._id,
-      busPathName: busPath.name
-    })
-  })
+  const hotelsSelects = map(busPaths, (busPath, index) => ({
+    options,
+    values: map(
+      intersectionBy(busPath.hotels, bookingsHotelsSummary, '_id'),
+      hotel => ({ value: hotel._id, label: hotel.name })
+    ),
+    busPathId: busPath._id,
+    busPathName: busPath.name
+  }))
   return {
     ...state,
     bookingsHotelsSummary,
