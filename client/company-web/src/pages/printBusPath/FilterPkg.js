a295222e78557e26eb25782a5fe75d2c3c944d55
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 15:36:03 +0700

    add filter pkg

---
+++ client/company-web/src/pages/printBusPath/FilterPkg.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+import Select from 'react-select'
+
+class FilterPkg extends PureComponent {
+  render() {
+    return <Select />
+  }
+}
+
+export default FilterPkg
