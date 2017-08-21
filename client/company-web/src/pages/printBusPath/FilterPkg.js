9b3d90a233244bbf009daf7a09e699c2f36aaf43
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 18:31:41 +0700

    implement filterpkg

--- client/company-web/src/pages/printBusPath/FilterPkg.js
+++ client/company-web/src/pages/printBusPath/FilterPkg.js
@@ -1,10 +1,42 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import map from 'lodash/map'
 import Select from 'react-select'
 
+import * as actions from '../../actions'
+
 class FilterPkg extends PureComponent {
+  componentDidMount() {
+    this.props.fetchPkgs()
+  }
+
   render() {
-    return <Select />
+    const {
+      pkgs,
+      date,
+      pkg,
+      fetchBookingsHotelsSummaryAndBusPaths
+    } = this.props
+    return (
+      <Select
+        options={pkgs}
+        onChange={value => fetchBookingsHotelsSummaryAndBusPaths(date, value)}
+        value={pkg}
+      />
+    )
   }
 }
 
-export default FilterPkg
+const mapStateToProps = ({
+  pkg: { pkgs },
+  printBusPath: { visibilityFilter: { date, pkg } }
+}) => ({
+  pkgs: map(pkgs, pkg => ({
+    value: pkg._id,
+    label: pkg.name
+  })),
+  date,
+  pkg
+})
+
+export default connect(mapStateToProps, actions)(FilterPkg)
