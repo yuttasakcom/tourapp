a8ffab2b8ff7c140550b77e9e763d1123183fd47
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 14:03:37 +0700

    promote busPath to model

--- server/src/models/busPath.js
+++ server/src/models/busPath.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const busPathSchema = new Schema({
-  name: String,
-  description: String,
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: 'Company'
+  },
   pkg: {
     type: Schema.Types.ObjectId,
     ref: 'Pkg'
   },
+  name: String,
+  description: String,
   hotels: [
     {
       type: Schema.Types.ObjectId,
@@ -17,4 +21,6 @@ const busPathSchema = new Schema({
   ]
 })
 
-module.exports = busPathSchema
+const BusPath = mongoose.model('BusPath', busPathSchema)
+
+module.exports = BusPath
