4a5b6f922067a0e99b6bfb877fbfec50047e6b97
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 14:26:57 +0700

    fix

--- server/src/repositories/company/companyGetBusPathsList.js
+++ server/src/repositories/company/companyGetBusPathsList.js
@@ -1,8 +1,4 @@
-const Company = require('../../models/company')
+const BusPath = require('../../models/busPath')
 
-module.exports = async (companyId, pkgId) => {
-  const { busPaths } = await Company.findById(companyId, {
-    busPaths: { $elemMatch: { pkg: pkgId } }
-  }).populate('busPaths.hotels')
-  return busPaths
-}
+module.exports = (companyId, pkgId) =>
+  BusPath.find({ company: companyId, pkg: pkgId }).populate('hotels')
