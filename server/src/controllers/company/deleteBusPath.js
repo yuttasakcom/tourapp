1b6cfcf48fee5e3a2c77ef7ecd72e9ae93335444
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 14:51:00 +0700

    fix delete

--- server/src/controllers/company/deleteBusPath.js
+++ server/src/controllers/company/deleteBusPath.js
@@ -1,8 +1,7 @@
 const repo = require('../../repositories')
 
 module.exports = async (req, res, next) => {
-  const companyId = req.user._id
   const busPathId = req.params.id
-  await repo.companyDeleteBusPath(companyId, busPathId)
+  await repo.companyDeleteBusPath(busPathId)
   return res.send({ message: 'Delete bus path completed' })
 }
