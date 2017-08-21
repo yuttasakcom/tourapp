0496991a535a3d0a62c659938c7982f923d2c5d5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 07:41:31 +0700

    wireup manage bus path

---
+++ client/company-web/src/pages/manageBusPath/index.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+
+import Card from '../../components/Card'
+import PkgDataTable from './PkgDataTable'
+import BusPathsModal from './busPathsModal'
+import * as actions from '../../actions'
+
+class ManageBusPath extends PureComponent {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Card title="Bus Paths">
+          <div className="row">
+            <div className="col-md-12">
+              <PkgDataTable />
+            </div>
+          </div>
+        </Card>
+        <BusPathsModal />
+      </div>
+    )
+  }
+}
+
+export default connect(null, actions)(ManageBusPath)
