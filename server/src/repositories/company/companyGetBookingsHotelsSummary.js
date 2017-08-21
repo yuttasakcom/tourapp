bda385f47fa14470b180bf7c00762dd945ad2d13
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-21 17:55:15 +0700

    add filter by pkg

--- server/src/repositories/company/companyGetBookingsHotelsSummary.js
+++ server/src/repositories/company/companyGetBookingsHotelsSummary.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 const Booking = require('../../models/booking')
 const Hotel = require('../../models/hotel')
 
-module.exports = async (companyId, dateStart, dateEnd) => {
+module.exports = async (companyId, pkgId, dateStart, dateEnd) => {
   const gteDate = moment(parseInt(dateStart, 10)).startOf('d').toDate()
   const ltDate = moment(parseInt(dateEnd, 10)).startOf('d').toDate()
   const results = await Booking.aggregate([
@@ -11,6 +11,7 @@ module.exports = async (companyId, dateStart, dateEnd) => {
       $match: {
         $and: [
           { company: ObjectId(companyId) },
+          { 'pkg._id': ObjectId(pkgId) },
           { 'tourist.date': { $gte: gteDate, $lt: ltDate } }
         ]
       }
