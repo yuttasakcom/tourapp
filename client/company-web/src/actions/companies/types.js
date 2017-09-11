dd1796efad3d9af872055c136ee08969c2173990
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-11 11:15:27 +0700

    add prefix to company action types

--- client/company-web/src/actions/companies/types.js
+++ client/company-web/src/actions/companies/types.js
@@ -1,84 +1,112 @@
-export const SIGN_IN_SUCCESS = 'sign_in_success'
-export const SIGN_OUT_SUCCESS = 'sign_out_success'
-export const SIGN_UP_SUCCESS = 'sign_up_success'
+export const COMPANY_SIGN_IN_SUCCESS = 'company_sign_in_success'
+export const COMPANY_SIGN_OUT_SUCCESS = 'company_sign_out_success'
+export const COMPANY_SIGN_UP_SUCCESS = 'company_sign_up_success'
 
-export const FETCH_DASHBOARD_SUCCESS = 'fetch_dashboard_success'
+export const COMPANY_FETCH_DASHBOARD_SUCCESS = 'company_fetch_dashboard_success'
 
-export const FETCH_BOOKINGS_SUCCESS = 'fetch_bookings_success'
-export const SET_BOOKINGS_STATUS_VISIBILITY_FILTER =
-  'set_bookings_status_visibility_filter'
-export const OPEN_MANAGE_BOOKING_MODAL = 'open_manage_booking_modal'
-export const CLOSE_MANAGE_BOOKING_MODAL = 'close_manage_booking_modal'
-export const ACCEPT_BOOKING_SUCCESS = 'accept_booking_success'
-export const REJECT_BOOKING_SUCCESS = 'reject_booking_success'
-export const COMPLETE_BOOKING_SUCCESS = 'complete_booking_success'
-export const BOOKING_NOTIFY = 'booking_notify'
+export const COMPANY_FETCH_BOOKINGS_SUCCESS = 'company_fetch_bookings_success'
+export const COMPANY_SET_BOOKINGS_STATUS_VISIBILITY_FILTER =
+  'company_set_bookings_status_visibility_filter'
+export const COMPANY_OPEN_MANAGE_BOOKING_MODAL =
+  'company_open_manage_booking_modal'
+export const COMPANY_CLOSE_MANAGE_BOOKING_MODAL =
+  'company_close_manage_booking_modal'
+export const COMPANY_ACCEPT_BOOKING_SUCCESS = 'company_accept_booking_success'
+export const COMPANY_REJECT_BOOKING_SUCCESS = 'company_reject_booking_success'
+export const COMPANY_COMPLETE_BOOKING_SUCCESS =
+  'company_complete_booking_success'
+export const COMPANY_BOOKING_NOTIFY = 'company_booking_notify'
 
-export const FETCH_PKGS_SUCCESS = 'fetch_pkgs_success'
-export const DELETE_PKG_SUCCESS = 'delete_pkg_success'
-export const ADD_PKG_SUCCESS = 'add_pkg_success'
-export const OPEN_ADD_PKG_MODAL = 'open_add_pkg_modal'
-export const CLOSE_ADD_PKG_MODAL = 'close_add_pkg_modal'
-export const OPEN_EDIT_PKG_MODAL = 'open_edit_pkg_modal'
-export const CLOSE_EDIT_PKG_MODAL = 'close_edit_pkg_modal'
-export const EDIT_PKG_SUCCESS = 'edit_pkg_success'
-export const OPEN_DELETE_PKG_MODAL = 'open_delete_pkg_modal'
-export const CLOSE_DELETE_PKG_MODAL = 'close_delete_pkg_modal'
+export const COMPANY_FETCH_PKGS_SUCCESS = 'company_fetch_pkgs_success'
+export const COMPANY_DELETE_PKG_SUCCESS = 'company_delete_pkg_success'
+export const COMPANY_ADD_PKG_SUCCESS = 'company_add_pkg_success'
+export const COMPANY_OPEN_ADD_PKG_MODAL = 'company_open_add_pkg_modal'
+export const COMPANY_CLOSE_ADD_PKG_MODAL = 'company_close_add_pkg_modal'
+export const COMPANY_OPEN_EDIT_PKG_MODAL = 'company_open_edit_pkg_modal'
+export const COMPANY_CLOSE_EDIT_PKG_MODAL = 'company_close_edit_pkg_modal'
+export const COMPANY_EDIT_PKG_SUCCESS = 'company_edit_pkg_success'
+export const COMPANY_OPEN_DELETE_PKG_MODAL = 'company_open_delete_pkg_modal'
+export const COMPANY_CLOSE_DELETE_PKG_MODAL = 'company_close_delete_pkg_modal'
 
-export const FETCH_AGENTS_SUCCESS = 'fetch_agents_success'
-export const DELETE_AGENT_SUCCESS = 'delete_agent_success'
-export const REQUEST_AGENT_SUCCESS = 'request_agent_success'
-export const CANCEL_REQUEST_AGENT_SUCCESS = 'cancel_request_agent_success'
-export const REJECT_REQUEST_AGENT_SUCCESS = 'reject_request_agent_success'
-export const ACCEPT_AGENT_SUCCESS = 'accept_agent_success'
-export const OPEN_REQUEST_AGENT_MODAL = 'open_request_agent_modal'
-export const CLOSE_REQUEST_AGENT_MODAL = 'close_request_agent_modal'
-export const OPEN_DELETE_AGENT_MODAL = 'open_delete_agent_modal'
-export const CLOSE_DELETE_AGENT_MODAL = 'close_delete_agent_modal'
-export const OPEN_RESET_PRICE_MODAL = 'open_reset_price_modal'
-export const CLOSE_RESET_PRICE_MODAL = 'close_reset_price_modal'
-export const FETCH_AGENT_CONTRACT_RATES_SUCCESS =
-  'fetch_agent_contract_rates_success'
+export const COMPANY_FETCH_AGENTS_SUCCESS = 'company_fetch_agents_success'
+export const COMPANY_DELETE_AGENT_SUCCESS = 'company_delete_agent_success'
+export const COMPANY_REQUEST_AGENT_SUCCESS = 'company_request_agent_success'
+export const COMPANY_CANCEL_REQUEST_AGENT_SUCCESS =
+  'company_cancel_request_agent_success'
+export const COMPANY_REJECT_REQUEST_AGENT_SUCCESS =
+  'company_reject_request_agent_success'
+export const COMPANY_ACCEPT_AGENT_SUCCESS = 'company_accept_agent_success'
+export const COMPANY_OPEN_REQUEST_AGENT_MODAL =
+  'company_open_request_agent_modal'
+export const COMPANY_CLOSE_REQUEST_AGENT_MODAL =
+  'company_close_request_agent_modal'
+export const COMPANY_OPEN_DELETE_AGENT_MODAL = 'company_open_delete_agent_modal'
+export const COMPANY_CLOSE_DELETE_AGENT_MODAL =
+  'company_close_delete_agent_modal'
+export const COMPANY_OPEN_RESET_PRICE_MODAL = 'company_open_reset_price_modal'
+export const COMPANY_CLOSE_RESET_PRICE_MODAL = 'company_close_reset_price_modal'
+export const COMPANY_FETCH_AGENT_CONTRACT_RATES_SUCCESS =
+  'company_fetch_agent_contract_rates_success'
 
-export const OPEN_CONTRACT_RATE_MODAL = 'open_contract_rate_modal'
-export const CLOSE_CONTRACT_RATE_MODAL = 'close_contract_rate_modal'
-export const OPEN_OFFER_SPECIAL_PRICE_MODAL = 'open_offer_special_price_modal'
-export const CLOSE_OFFER_SPECIAL_PRICE_MODAL = 'close_offer_special_price_modal'
-export const OFFER_SPECIAL_PRICE_SUCCESS = 'offer_special_price_success'
-export const RESET_PRICE_SUCCESS = 'reset_price_success'
+export const COMPANY_OPEN_CONTRACT_RATE_MODAL =
+  'company_open_contract_rate_modal'
+export const COMPANY_CLOSE_CONTRACT_RATE_MODAL =
+  'company_close_contract_rate_modal'
+export const COMPANY_OPEN_OFFER_SPECIAL_PRICE_MODAL =
+  'company_open_offer_special_price_modal'
+export const COMPANY_CLOSE_OFFER_SPECIAL_PRICE_MODAL =
+  'company_close_offer_special_price_modal'
+export const COMPANY_OFFER_SPECIAL_PRICE_SUCCESS =
+  'company_offer_special_price_success'
+export const COMPANY_RESET_PRICE_SUCCESS = 'company_reset_price_success'
 
-export const OPEN_VIEW_AGENT_PROFILE_MODAL = 'open_view_agent_profile_modal'
-export const CLOSE_VIEW_AGENT_PROFILE_MODAL = 'close_view_agent_profile_modal'
-export const TOGGLE_PROFILE_MENU = 'toggle_profile_menu'
-export const TOGGLE_NOTIFICATION_GEM = 'toggle_notification_gem'
-export const TOGGLE_ACCEPT_PENDING_GEM = 'toggle_accept_pending_gem'
-export const TOGGLE_REQUEST_PENDING_GEM = 'toggle_request_pending_gem'
-export const HIDE_ALL_GEM = 'hide_all_gem'
-export const FETCH_REQUEST_PENDINGS_SUCCESS = 'fetch_request_pendings_success'
-export const FETCH_ACCEPT_PENDINGS_SUCCESS = 'fetch_accept_pendings_success'
-export const FETCH_NOTIFICATIONS_SUCCESS = 'fetch_notifications_success'
-export const ADD_NOTIFICATION_SUCCESS = 'add_notification_success'
+export const COMPANY_OPEN_VIEW_AGENT_PROFILE_MODAL =
+  'company_open_view_agent_profile_modal'
+export const COMPANY_CLOSE_VIEW_AGENT_PROFILE_MODAL =
+  'company_close_view_agent_profile_modal'
+export const COMPANY_TOGGLE_PROFILE_MENU = 'company_toggle_profile_menu'
+export const COMPANY_TOGGLE_NOTIFICATION_GEM = 'company_toggle_notification_gem'
+export const COMPANY_TOGGLE_ACCEPT_PENDING_GEM =
+  'company_toggle_accept_pending_gem'
+export const COMPANY_TOGGLE_REQUEST_PENDING_GEM =
+  'company_toggle_request_pending_gem'
+export const COMPANY_HIDE_ALL_GEM = 'company_hide_all_gem'
+export const COMPANY_FETCH_REQUEST_PENDINGS_SUCCESS =
+  'company_fetch_request_pendings_success'
+export const COMPANY_FETCH_ACCEPT_PENDINGS_SUCCESS =
+  'company_fetch_accept_pendings_success'
+export const COMPANY_FETCH_NOTIFICATIONS_SUCCESS =
+  'company_fetch_notifications_success'
+export const COMPANY_ADD_NOTIFICATION_SUCCESS =
+  'company_add_notification_success'
 
-export const FETCH_BOOKINGS_SUMMARY_SUCCESS = 'fetch_bookings_summary_success'
+export const COMPANY_FETCH_BOOKINGS_SUMMARY_SUCCESS =
+  'company_fetch_bookings_summary_success'
 
-export const FETCH_BOOKINGS_HOTELS_SUMMARY_AND_BUS_PATHS_SUCCESS =
-  'fetch_bookings_hotels_summary_and_bus_paths_success'
-export const MANAGE_BUS_PATH = 'manage_bus_path'
+export const COMPANY_FETCH_BOOKINGS_HOTELS_SUMMARY_AND_BUS_PATHS_SUCCESS =
+  'company_fetch_bookings_hotels_summary_and_bus_paths_success'
+export const COMPANY_MANAGE_BUS_PATH = 'company_manage_bus_path'
 
-export const TOGGLE_MENU = 'toggle_menu'
-export const CLOSE_MENU = 'close_menu'
+export const COMPANY_TOGGLE_MENU = 'company_toggle_menu'
+export const COMPANY_CLOSE_MENU = 'company_close_menu'
 
-export const FETCH_BUS_PATH_HOTELS_SUCCESS = 'fetch_bus_path_hotels_success'
-export const FETCH_BUS_PATHS_SUCCESS = 'fetch_bus_paths_success'
-export const DELETE_BUS_PATH_SUCCESS = 'delete_bus_path_success'
-export const ADD_BUS_PATH_SUCCESS = 'add_bus_path_success'
-export const OPEN_ADD_BUS_PATH_MODAL = 'open_add_bus_path_modal'
-export const CLOSE_ADD_BUS_PATH_MODAL = 'close_add_bus_path_modal'
-export const OPEN_EDIT_BUS_PATH_MODAL = 'open_edit_bus_path_modal'
-export const CLOSE_EDIT_BUS_PATH_MODAL = 'close_edit_bus_path_modal'
-export const EDIT_BUS_PATH_SUCCESS = 'edit_bus_path_success'
-export const OPEN_DELETE_BUS_PATH_MODAL = 'open_delete_bus_path_modal'
-export const CLOSE_DELETE_BUS_PATH_MODAL = 'close_delete_bus_path_modal'
+export const COMPANY_FETCH_BUS_PATH_HOTELS_SUCCESS =
+  'company_fetch_bus_path_hotels_success'
+export const COMPANY_FETCH_BUS_PATHS_SUCCESS = 'company_fetch_bus_paths_success'
+export const COMPANY_DELETE_BUS_PATH_SUCCESS = 'company_delete_bus_path_success'
+export const COMPANY_ADD_BUS_PATH_SUCCESS = 'company_add_bus_path_success'
+export const COMPANY_OPEN_ADD_BUS_PATH_MODAL = 'company_open_add_bus_path_modal'
+export const COMPANY_CLOSE_ADD_BUS_PATH_MODAL =
+  'company_close_add_bus_path_modal'
+export const COMPANY_OPEN_EDIT_BUS_PATH_MODAL =
+  'company_open_edit_bus_path_modal'
+export const COMPANY_CLOSE_EDIT_BUS_PATH_MODAL =
+  'company_close_edit_bus_path_modal'
+export const COMPANY_EDIT_BUS_PATH_SUCCESS = 'company_edit_bus_path_success'
+export const COMPANY_OPEN_DELETE_BUS_PATH_MODAL =
+  'company_open_delete_bus_path_modal'
+export const COMPANY_CLOSE_DELETE_BUS_PATH_MODAL =
+  'company_close_delete_bus_path_modal'
 
-export const OPEN_BUS_PATHS_MODAL = 'open_bus_paths_modal'
-export const CLOSE_BUS_PATHS_MODAL = 'close_bus_paths_modal'
+export const COMPANY_OPEN_BUS_PATHS_MODAL = 'company_open_bus_paths_modal'
+export const COMPANY_CLOSE_BUS_PATHS_MODAL = 'company_close_bus_paths_modal'
