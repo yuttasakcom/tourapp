fdbdce817d2952d33aacd57b40f592f473aeb464
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-11 15:17:12 +0700

    add auth action type

--- client/company-web/src/actions/types.js
+++ client/company-web/src/actions/types.js
@@ -1,2 +1,7 @@
 export const TOGGLE_MENU = 'toggle_menu'
 export const CLOSE_MENU = 'close_menu'
+
+export const SIGN_IN_SUCCESS = 'sign_in_success'
+export const SIGN_OUT_SUCCESS = 'sign_out_success'
+export const SIGN_UP_SUCCESS = 'sign_up_success'
+export const TOGGLE_PROFILE_MENU = 'toggle_profile_menu'
