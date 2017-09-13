3feff9294a896c961cc933f0ef1ae90d3cfbf83f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-13 07:41:29 +0700

    add socket

--- client/company-web/src/pages/companies/layout/index.js
+++ client/company-web/src/pages/companies/layout/index.js
@@ -2,12 +2,14 @@ import React, { PureComponent } from 'react'
 
 import Layout from '../../../components/layout'
 import MenuList from './MenuList'
+import Socket from './Socket'
 
 class CompanyLayout extends PureComponent {
   render() {
     const { children } = this.props
     return (
       <Layout logo="COMPANY APP" MenuList={MenuList}>
+        <Socket />
         {children}
       </Layout>
     )
