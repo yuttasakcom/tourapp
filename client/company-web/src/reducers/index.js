5c9a8259f42ca41baa93e3982bc9291c7343e0b1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-13 09:01:27 +0700

    remove layout reducer

--- client/company-web/src/reducers/index.js
+++ client/company-web/src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 
-import layout from './layoutReducer'
 import auth from './authReducer'
 import company from './companies'
 import agent from './agents'
 
 export default combineReducers({
   auth,
-  layout,
   company,
   agent
 })
