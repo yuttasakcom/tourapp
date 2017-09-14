0ce1d705e314831c5ccbbd4cb8bd658e2cd3f239
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-14 15:12:52 +0700

    use redux

--- client/company-web/src/components/layout/mainMenu/MenuItem.js
+++ client/company-web/src/components/layout/mainMenu/MenuItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class MenuItem extends React.PureComponent {
   render() {
-    const { path, icon, text, location: { pathname } } = this.props
+    const { path, icon, text, currentPath } = this.props
     return (
-      <li className={path === pathname ? 'active' : ''}>
+      <li className={path === currentPath ? 'active' : ''}>
         <Link to={path}>
           <i className="material-icons">{icon}</i>
           <p>{text}</p>
@@ -15,4 +16,8 @@ class MenuItem extends React.PureComponent {
   }
 }
 
-export default withRouter(MenuItem)
+const mapStateToProps = state => {
+  return { currentPath: state.router.location.pathname }
+}
+
+export default connect(mapStateToProps)(MenuItem)
