d7f8ae5cadba2349437036d5f7de8c34a2d7b983
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-15 10:16:00 +0700

    add check role feature

--- client/company-web/src/containers/PrivateRoute.js
+++ client/company-web/src/containers/PrivateRoute.js
@@ -4,23 +4,37 @@ import { Route, Redirect, withRouter } from 'react-router-dom'
 
 class PrivateRoute extends PureComponent {
   render() {
-    const { component: Component, authenticated, ...rest } = this.props
+    const {
+      component: Component,
+      userRole,
+      authenticated,
+      user,
+      ...rest
+    } = this.props
     return (
       <Route
         {...rest}
         render={props =>
-          authenticated
-            ? <Component {...props} />
-            : <Redirect
-                to={{ pathname: '/signin', state: { from: props.location } }}
-              />}
+          authenticated && userRole === user.role ? (
+            <Component {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: `/${userRole === 'company'
+                  ? 'companies'
+                  : 'agents'}/signin`,
+                state: { from: props.location }
+              }}
+            />
+          )}
       />
     )
   }
 }
 
-const mapStateToProps = ({ auth: { authenticated } }) => {
-  return { authenticated }
-}
+const mapStateToProps = ({ auth: { authenticated, user } }) => ({
+  authenticated,
+  user
+})
 
 export default withRouter(connect(mapStateToProps)(PrivateRoute))
