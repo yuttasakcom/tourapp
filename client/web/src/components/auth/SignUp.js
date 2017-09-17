4b0d8c0be55cb86fa67d66b2028f7d62b2289866
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-17 11:14:21 +0700

    use redux form

--- client/web/src/components/auth/SignUp.js
+++ client/web/src/components/auth/SignUp.js
@@ -1,77 +1,51 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+import renderField from '../renderField'
 
 class SignUp extends React.PureComponent {
   render() {
-    const { active, onCancelClick } = this.props
+    const { active, onCancelClick, handleSubmit, submitting } = this.props
     return (
-      <div className={`tab-pane${active ? ' active' : ''}`} id="Registration">
-        <form role="form" className="form-horizontal">
-          <div className="form-group">
-            <label htmlFor="email" className="col-sm-2 control-label">
-              Name
-            </label>
-            <div className="col-sm-10">
-              <div className="row">
-                <div className="col-md-3">
-                  <select className="form-control">
-                    <option>Mr.</option>
-                    <option>Ms.</option>
-                    <option>Mrs.</option>
-                  </select>
-                </div>
-                <div className="col-md-9">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="email" className="col-sm-2 control-label">
-              Email
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="mobile" className="col-sm-2 control-label">
-              Mobile
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="email"
-                className="form-control"
-                id="mobile"
-                placeholder="Mobile"
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="col-sm-2 control-label">
-              Password
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Password"
+      <div className={`tab-pane${active ? ' active' : ''}`}>
+        <form onSubmit={handleSubmit} className="form-horizontal">
+          <div className="row">
+            <div className="col-md-offset-1 col-md-10">
+              <Field
+                name="email"
+                component={renderField}
+                label="Email"
+                type="text"
               />
             </div>
           </div>
           <div className="row">
-            <div className="col-sm-2" />
-            <div className="col-sm-10">
-              <button type="button" className="btn btn-primary btn-sm">
+            <div className="col-md-offset-1 col-md-10">
+              <Field
+                name="password"
+                component={renderField}
+                label="Password"
+                type="password"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-offset-1 col-md-10">
+              <Field
+                name="name"
+                component={renderField}
+                label="Name"
+                type="text"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-offset-1 col-sm-10">
+              <button
+                type="submit"
+                disabled={submitting}
+                className="btn btn-primary btn-sm"
+              >
                 Save & Continue
               </button>
               <button
@@ -89,4 +63,17 @@ class SignUp extends React.PureComponent {
   }
 }
 
-export default SignUp
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  if (!values.password) {
+    errors.password = 'Required'
+  }
+  return errors
+}
+
+export default reduxForm({ form: 'signUp', validate })(SignUp)
