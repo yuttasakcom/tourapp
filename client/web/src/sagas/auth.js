dae423d1929b06d7a0ab3df58ed9d75076aeb88e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-21 10:05:33 +0700

    fix flow

--- client/web/src/sagas/auth.js
+++ client/web/src/sagas/auth.js
@@ -2,7 +2,7 @@ import { error } from 'react-notification-system-redux'
 import axios from 'axios'
 import jwtDecode from 'jwt-decode'
 import cookie from 'js-cookie'
-import { take, fork, call, put } from 'redux-saga/effects'
+import { take, call, put } from 'redux-saga/effects'
 
 import {
   SIGN_IN,
@@ -12,38 +12,46 @@ import {
   SIGN_OUT_SUCCESS
 } from '../actions/types'
 
-export function* authorize({ type, role, values }) {
-  try {
-    const { data: { token } } = yield call(
-      axios.post,
-      `/api/${role === 'company' ? 'companies' : 'agents'}/${type === SIGN_IN
-        ? 'signin'
-        : 'signup'}`,
-      {
-        ...values,
-        role
-      }
-    )
-    const user = yield call(jwtDecode, token)
-    yield put({ type: SIGN_IN_SUCCESS, payload: user })
-  } catch (e) {
-    yield put(
-      error({
-        title: 'แจ้งเตือน',
-        message: e.response.data
-      })
-    )
-  }
+export function* signOut() {
+  yield take(SIGN_OUT)
+  yield call(cookie.remove, 'jwt')
+  yield put({ type: SIGN_OUT_SUCCESS })
 }
 
 export default function* auth() {
   while (true) {
-    const action = yield take([SIGN_IN, SIGN_UP, SIGN_IN_SUCCESS])
-    action.type === SIGN_IN_SUCCESS
-      ? yield put({ type: SIGN_IN_SUCCESS, payload: action.payload })
-      : yield fork(authorize, action)
-    yield take(SIGN_OUT)
-    yield call(cookie.remove, 'jwt')
-    yield put({ type: SIGN_OUT_SUCCESS })
+    const { type, payload: { role, values } } = yield take([
+      SIGN_IN,
+      SIGN_UP,
+      SIGN_IN_SUCCESS
+    ])
+
+    if (type !== SIGN_IN_SUCCESS) {
+      try {
+        const { data: { token } } = yield call(
+          axios.post,
+          `/api/${role === 'company' ? 'companies' : 'agents'}/${type ===
+          SIGN_IN
+            ? 'signin'
+            : 'signup'}`,
+          {
+            ...values,
+            role
+          }
+        )
+        const user = yield call(jwtDecode, token)
+        yield put({ type: SIGN_IN_SUCCESS, payload: user })
+        yield signOut()
+      } catch (e) {
+        yield put(
+          error({
+            title: 'แจ้งเตือน',
+            message: e.response.data
+          })
+        )
+      }
+    } else {
+      yield signOut()
+    }
   }
 }
