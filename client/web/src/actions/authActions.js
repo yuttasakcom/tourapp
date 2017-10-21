d34bdb0c54e0b666b9f2b4507213d9e6389629b4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-21 10:05:00 +0700

    refactor for convention

--- client/web/src/actions/authActions.js
+++ client/web/src/actions/authActions.js
@@ -1,5 +1,11 @@
 import { SIGN_UP, SIGN_IN, SIGN_OUT } from './types'
 
-export const signIn = (role, values) => ({ type: SIGN_IN, role, values })
-export const signUp = (role, values) => ({ type: SIGN_UP, role, values })
+export const signIn = (role, values) => ({
+  type: SIGN_IN,
+  payload: { role, values }
+})
+export const signUp = (role, values) => ({
+  type: SIGN_UP,
+  payload: { role, values }
+})
 export const signOut = () => ({ type: SIGN_OUT })
