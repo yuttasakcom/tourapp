e1e442c4c422c732cc0b311dd62da06ac75c2e86
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-21 13:15:48 +0700

    refactor

--- client/web/src/sagas/__test__/auth.test.js
+++ client/web/src/sagas/__test__/auth.test.js
@@ -45,53 +45,73 @@ describe('auth', () => {
     data.signInSuccess = data.signIn.clone()
   })
 
-  it('sign_in_success must be take sign_out and take again', () => {
-    expect(data.signInSuccess.next({ type: SIGN_IN_SUCCESS }).value).toEqual(
-      call(signOut)
-    )
-    expect(data.signInSuccess.next().value).toEqual(
-      take([SIGN_IN, SIGN_UP, SIGN_IN_SUCCESS])
-    )
+  describe('sign in success firt time', () => {
+    it('sign_in_success must be take sign_out', () => {
+      expect(data.signInSuccess.next({ type: SIGN_IN_SUCCESS }).value).toEqual(
+        call(signOut)
+      )
+    })
+
+    it('take auth again', () => {
+      expect(data.signInSuccess.next().value).toEqual(
+        take([SIGN_IN, SIGN_UP, SIGN_IN_SUCCESS])
+      )
+    })
   })
 
-  it('sign_in must be call correct signin api', () => {
-    const action = {
-      type: SIGN_IN,
-      payload: {
-        role: 'company',
-        values: { username: 'test' }
+  describe('signIn', () => {
+    it('sign_in must be call correct signin api', () => {
+      const action = {
+        type: SIGN_IN,
+        payload: {
+          role: 'company',
+          values: { username: 'test' }
+        }
       }
-    }
-    expect(data.signIn.next(action).value).toEqual(
-      call(axios.post, `/api/companies/signin`, {
-        ...action.payload.values,
-        role: action.payload.role
-      })
-    )
+      expect(data.signIn.next(action).value).toEqual(
+        call(axios.post, `/api/companies/signin`, {
+          ...action.payload.values,
+          role: action.payload.role
+        })
+      )
+    })
+
+    it('signin api success must be call jwtDecode with token', () => {
+      const res = {
+        data: {
+          token: 'token'
+        }
+      }
+      expect(data.signIn.next(res).value).toEqual(
+        call(jwtDecode, res.data.token)
+      )
+    })
+
+    it('put sign_in_success with user object and call signOut', () => {
+      const user = {
+        name: 'paiboon'
+      }
+      expect(data.signIn.next(user).value).toEqual(
+        put({ type: SIGN_IN_SUCCESS, payload: user })
+      )
+    })
   })
 
-  it('signin api success must be call jwtDecode with token', () => {
-    const res = {
-      data: {
-        token: 'token'
+  describe('signUp', () => {
+    it('sign_up must be call correct signup api', () => {
+      const action = {
+        type: SIGN_UP,
+        payload: {
+          role: 'agent',
+          values: { username: 'test' }
+        }
       }
-    }
-    expect(data.signIn.next(res).value).toEqual(call(jwtDecode, res.data.token))
-  })
-
-  it('sign_up must be call correct signup api', () => {
-    const action = {
-      type: SIGN_UP,
-      payload: {
-        role: 'agent',
-        values: { username: 'test' }
-      }
-    }
-    expect(data.signUp.next(action).value).toEqual(
-      call(axios.post, `/api/agents/signup`, {
-        ...action.payload.values,
-        role: action.payload.role
-      })
-    )
+      expect(data.signUp.next(action).value).toEqual(
+        call(axios.post, `/api/agents/signup`, {
+          ...action.payload.values,
+          role: action.payload.role
+        })
+      )
+    })
   })
 })
