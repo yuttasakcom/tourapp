42d1d84e49a4bac883f4c224a4baee8d761b6753
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-21 09:02:27 +0700

    add auth type for saga

--- client/web/src/actions/types.js
+++ client/web/src/actions/types.js
@@ -1,3 +1,6 @@
+export const SIGN_IN = 'sign_in'
+export const SIGN_OUT = 'sign_out'
+export const SIGN_UP = 'sign_up'
 export const SIGN_IN_SUCCESS = 'sign_in_success'
 export const SIGN_OUT_SUCCESS = 'sign_out_success'
 export const SIGN_UP_SUCCESS = 'sign_up_success'
