2fb89dd012b03d26a57e0feeec549cb00e5c4ca0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-22 15:35:28 +0700

    remove lagacy auth actions

--- client/web/src/actions/authActions.js
+++
@@ -1,7 +0,0 @@
-import { createActions } from 'redux-actions'
-
-export default createActions({
-  SIGN_IN: payload => payload,
-  SIGN_UP: payload => payload,
-  SIGN_OUT: null
-})
--- client/web/src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const SIGN_IN = 'SIGN_IN'
-export const SIGN_OUT = 'SIGN_OUT'
-export const SIGN_UP = 'sign_up'
-export const SIGN_IN_SUCCESS = 'sign_in_success'
-export const SIGN_OUT_SUCCESS = 'sign_out_success'
-export const SIGN_UP_SUCCESS = 'sign_up_success'
