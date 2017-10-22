c2e9f668789aebef041a8a3ec8e422f5a8d70976
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-22 15:04:37 +0700

    add new auth reducer

---
+++ client/web/src/state/ducks/auth/reducers.js
@@ -0,0 +1,25 @@
+import { handleActions } from 'redux-actions'
+
+import { SIGN_IN_SUCCESS, SIGN_OUT_SUCCESS, SIGN_UP_SUCCESS } from './types'
+
+const initialState = {
+  authenticated: false,
+  user: ''
+}
+
+export default handleActions(
+  {
+    [SIGN_IN_SUCCESS]: (state, action) => ({
+      ...state,
+      authenticated: true,
+      user: action.payload
+    }),
+    [SIGN_UP_SUCCESS]: (state, action) => ({
+      ...state,
+      authenticated: true,
+      user: action.payload
+    }),
+    [SIGN_OUT_SUCCESS]: (state, action) => ({ ...state, authenticated: false })
+  },
+  initialState
+)
