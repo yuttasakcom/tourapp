6a3c7a0858ac3b6023be4bc2ea7847dd06e835aa
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-24 13:51:12 +0700

    add edit pkg saga

--- client/web/src/state/ducks/companies/pkg/sagas.js
+++ client/web/src/state/ducks/companies/pkg/sagas.js
@@ -3,7 +3,7 @@ import { takeEvery, put, call, all } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosCompanies'
 import actions from '../../actions'
-import { FETCH_PKGS, ADD_PKG } from './types'
+import { FETCH_PKGS, ADD_PKG, EDIT_PKG } from './types'
 
 export function* watchFetchPkgs() {
   yield takeEvery(FETCH_PKGS, function*() {
@@ -43,6 +43,29 @@ export function* watchAddPkg() {
   })
 }
 
+export function* watchEditPkg() {
+  yield takeEvery(EDIT_PKG, function*(action) {
+    const { id, values } = action.payload
+    try {
+      const { data } = yield call(axios.put, `/pkgs/${id}`, values)
+      yield put(actions.company.pkg.editPkgSuccess(data))
+      yield put(
+        success({
+          title: 'แจ้งเตือน',
+          message: 'แก้ไขแพ็คเกจเรียบร้อยแล้ว!'
+        })
+      )
+    } catch (e) {
+      yield put(
+        error({
+          title: 'แจ้งเตือน',
+          message: e.response.data
+        })
+      )
+    }
+  })
+}
+
 export default function* rootSaga() {
-  yield all([watchFetchPkgs(), watchAddPkg()])
+  yield all([watchFetchPkgs(), watchAddPkg(), watchEditPkg()])
 }
