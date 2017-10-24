8eec6047b78a7052b5178bfa3b0bd7e359092bc4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-24 16:20:14 +0700

    wireup rootBusPathsSaga

--- client/web/src/state/ducks/companies/sagas.js
+++ client/web/src/state/ducks/companies/sagas.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects'
 
 import rootPkgSaga from './pkg/sagas'
+import rootBusPathSaga from './busPath/sagas'
 
 export default function* rootSaga() {
-  yield all([rootPkgSaga()])
+  yield all([rootPkgSaga(), rootBusPathSaga()])
 }
