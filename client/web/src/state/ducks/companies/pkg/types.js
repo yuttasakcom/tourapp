f06467ffa52845d9cc01b1a48cd23cca9b643a49
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-24 09:55:46 +0700

    add company pkg type

---
+++ client/web/src/state/ducks/companies/pkg/types.js
@@ -0,0 +1,9 @@
+const def = action => `COMPANY/PKG/${action}`
+export const FETCH_PKGS = def('FETCH_PKGS')
+export const FETCH_PKGS_SUCCESS = def('FETCH_PKGS_SUCCESS')
+export const ADD_PKG = def('ADD_PKG')
+export const ADD_PKG_SUCCESS = def('ADD_PKG_SUCCESS')
+export const EDIT_PKG = def('EDIT_PKG')
+export const EDIT_PKG_SUCCESS = def('EDIT_PKG_SUCCESS')
+export const DELETE_PKG = def('DELETE_PKG')
+export const DELETE_PKG_SUCCESS = def('DELETE_PKG_SUCCESS')
