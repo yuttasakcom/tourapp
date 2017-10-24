8dac8ced792d500fc94fdf98a2e9d74fd39087c1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-24 14:54:41 +0700

    wireup busPath action

--- client/web/src/state/ducks/actions.js
+++ client/web/src/state/ducks/actions.js
@@ -2,10 +2,12 @@ import { createActions } from 'redux-actions'
 
 import auth from './auth/actions'
 import companyPkg from './companies/pkg/actions'
+import companyBusPath from './companies/busPath/actions'
 
 export default createActions({
   COMPANY: {
-    PKG: companyPkg
+    PKG: companyPkg,
+    BUS_PATH: companyBusPath
   },
   AGENT: {},
   COMMON: {
