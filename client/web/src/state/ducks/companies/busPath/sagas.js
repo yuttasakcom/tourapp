fd09864072426955911047a2259907f812a72662
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-24 16:32:43 +0700

    add fetchBusPathHotels saga

--- client/web/src/state/ducks/companies/busPath/sagas.js
+++ client/web/src/state/ducks/companies/busPath/sagas.js
@@ -1,5 +1,5 @@
 import { error, success } from 'react-notification-system-redux'
-import { takeEvery, put, call, all } from 'redux-saga/effects'
+import { takeEvery, put, call, all, select } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosCompanies'
 import actions from '../../actions'
@@ -24,6 +24,26 @@ export function* watchFetchBusPaths() {
         error({
           title: 'แจ้งเตือน',
           message: e.response.data
+        })
+      )
+    }
+  })
+}
+
+export function* watchFetchBusPathHotels() {
+  yield takeEvery(FETCH_BUS_PATH_HOTELS, function*(action) {
+    const selectedPkg = yield select(state => state.company.busPath.selectedPkg)
+    try {
+      const { data } = yield call(
+        axios.get,
+        `/bus-path-hotels/${action.payload}?pkgId=${selectedPkg}`
+      )
+      yield put(actions.company.busPath.fetchBusPathHotelsSuccess(data))
+    } catch (e) {
+      yield put(
+        error({
+          title: 'แจ้งเตือน',
+          message: e.message
         })
       )
     }
