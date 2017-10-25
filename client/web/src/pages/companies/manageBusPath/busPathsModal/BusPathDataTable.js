b2d8d848db742b5fafb0fc060ab70e6f466a8587
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-25 14:04:01 +0700

    add modal state

--- client/web/src/pages/companies/manageBusPath/busPathsModal/BusPathDataTable.js
+++ client/web/src/pages/companies/manageBusPath/busPathsModal/BusPathDataTable.js
@@ -5,12 +5,28 @@ import DeleteModal from './DeleteModal'
 import EditModal from './EditModal'
 
 class BusPathDataTable extends PureComponent {
+  state = {
+    showEditBusPathModal: false,
+    showDeleteBusPathModal: false
+  }
+
   render() {
     return (
       <div>
-        <Table />
-        <EditModal />
-        <DeleteModal />
+        <Table
+          openEditBusPathModal={() =>
+            this.setState({ showEditBusPathModal: true })}
+          openDeleteBusPathModal={() =>
+            this.setState({ showDeleteBusPathModal: true })}
+        />
+        <EditModal
+          showModal={this.state.showEditBusPathModal}
+          closeModal={() => this.setState({ showEditBusPathModal: false })}
+        />
+        <DeleteModal
+          showModal={this.state.showDeleteBusPathModal}
+          closeModal={() => this.setState({ showDeleteBusPathModal: false })}
+        />
       </div>
     )
   }
