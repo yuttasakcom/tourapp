f6227575232fc44923521c2e2d4b224633b821a9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-25 16:35:14 +0700

    add print bus path actions

--- client/web/src/state/ducks/actions.js
+++ client/web/src/state/ducks/actions.js
@@ -3,11 +3,13 @@ import { createActions } from 'redux-actions'
 import auth from './auth/actions'
 import companyPkg from './companies/pkg/actions'
 import companyBusPath from './companies/busPath/actions'
+import companyPrintBusPath from './companies/printBusPath/actions'
 
 export default createActions({
   COMPANY: {
     PKG: companyPkg,
-    BUS_PATH: companyBusPath
+    BUS_PATH: companyBusPath,
+    PRINT_BUS_PATH: companyPrintBusPath
   },
   AGENT: {},
   COMMON: {
