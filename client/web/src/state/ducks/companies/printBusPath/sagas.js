fa57580fe983e1520fcf8b4ed5fbd195aea42b50
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-25 15:46:20 +0700

    add update bus paths saga

--- client/web/src/state/ducks/companies/printBusPath/sagas.js
+++ client/web/src/state/ducks/companies/printBusPath/sagas.js
@@ -1,10 +1,11 @@
+import map from 'lodash/map'
 import { error, success } from 'react-notification-system-redux'
 import { takeEvery, put, call, all, select } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosCompanies'
 import actions from '../../actions'
 import { openCompanyReport } from '../../../utils'
-import { PRINT_BUS_PATHS } from './types'
+import { PRINT_BUS_PATHS, UPDATE_BUS_PATHS } from './types'
 
 export function* watchPrintBusPaths() {
   yield takeEvery(PRINT_BUS_PATHS, function*() {
@@ -12,6 +13,26 @@ export function* watchPrintBusPaths() {
       state => state.company.printBusPath.visibilityFilter
     )
     openCompanyReport(`bus-paths-summary/${date}?pkgId=${pkg.value}`)
+  })
+}
+
+export function* watchUpdateBusPaths() {
+  yield takeEvery(UPDATE_BUS_PATHS, function*() {
+    const hotelsSelects = yield select(
+      state => state.company.printBusPath.hotelsSelects
+    )
+    const busPathsProps = map(hotelsSelects, hotelsSelect => ({
+      busPathId: hotelsSelect.busPathId,
+      hotelIds: map(hotelsSelect.values, 'value'),
+      removedHotelIds: hotelsSelect.removedHotelIds
+    }))
+    yield call(axios.put, '/bus-paths', busPathsProps)
+    yield put(
+      success({
+        title: 'แจ้งเตือน',
+        message: 'อัพเดทสายรถเรียบร้อยแล้ว!'
+      })
+    )
   })
 }
 
