9a971747f70a864501de6a6a081814c002a414e9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-25 14:54:52 +0700

    add new manageBusPath

---
+++ client/web/src/state/ducks/companies/printBusPath/reducers/manageBusPath.js
@@ -0,0 +1,52 @@
+import omit from 'lodash/omit'
+import map from 'lodash/map'
+import difference from 'lodash/difference'
+import pick from 'lodash/pick'
+import merge from 'lodash/merge'
+
+export default (state, action) => {
+  const { values, index } = action.payload
+  const addMode = state.hotelsSelects[index].values
+    ? state.hotelsSelects[index].values.length < values.length
+    : true
+  if (addMode) {
+    return {
+      ...state,
+      hotelsSelects: map(state.hotelsSelects, (hotelsSelect, i) => ({
+        options:
+          Number(i) === index
+            ? hotelsSelect.options
+            : omit(hotelsSelect.options, values[values.length - 1].value),
+        values: Number(i) === index ? values : hotelsSelect.values,
+        busPathId: hotelsSelect.busPathId,
+        busPathName: hotelsSelect.busPathName,
+        removedHotelIds: [...hotelsSelect.removedHotelIds]
+      }))
+    }
+  } else {
+    const removedHotelIds = map(
+      difference(state.hotelsSelects[index].values, values),
+      'value'
+    )
+    const removedItemsOptions = pick(
+      state.hotelsSelects[index].options,
+      removedHotelIds
+    )
+    return {
+      ...state,
+      hotelsSelects: map(state.hotelsSelects, (hotelsSelect, i) => ({
+        options:
+          Number(i) === index
+            ? hotelsSelect.options
+            : merge(hotelsSelect.options, removedItemsOptions),
+        values: Number(i) === index ? values : hotelsSelect.values,
+        busPathId: hotelsSelect.busPathId,
+        busPathName: hotelsSelect.busPathName,
+        removedHotelIds:
+          Number(i) === index
+            ? [...hotelsSelect.removedHotelIds, ...removedHotelIds]
+            : [...hotelsSelect.removedHotelIds]
+      }))
+    }
+  }
+}
