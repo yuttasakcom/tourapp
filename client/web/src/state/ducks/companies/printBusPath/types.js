a265909ca9c453bd23b34fead12dc98340ceb6fe
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-25 15:40:59 +0700

    fix typo

--- client/web/src/state/ducks/companies/printBusPath/types.js
+++ client/web/src/state/ducks/companies/printBusPath/types.js
@@ -1,5 +1,5 @@
 const def = action => `COMPANY/PRINT_BUS_PATH/${action}`
-export const PRINT_BUS_PATH = def('PRINT_BUS_PATH')
+export const PRINT_BUS_PATHS = def('PRINT_BUS_PATHS')
 export const FETCH_BOOKINGS_HOTELS_SUMMARY_AND_BUS_PATHS = def(
   'FETCH_BOOKINGS_HOTELS_SUMMARY_AND_BUS_PATHS'
 )
