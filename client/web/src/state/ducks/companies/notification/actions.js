a006e3b584729496dd0436067b218b1f1739bc2d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 19:40:30 +0700

    add relationship action

--- client/web/src/state/ducks/companies/notification/actions.js
+++ client/web/src/state/ducks/companies/notification/actions.js
@@ -5,5 +5,11 @@ export default {
   FETCH_NOTIFICATIONS: null,
   FETCH_NOTIFICATIONS_SUCCESS: payload => payload,
   FETCH_REQUEST_PENDINGS: null,
-  FETCH_REQUEST_PENDINGS_SUCCESS: payload => payload
+  FETCH_REQUEST_PENDINGS_SUCCESS: payload => payload,
+  ACCEPT_AGENT: id => id,
+  ACCEPT_AGENT_SUCCESS: payload => payload,
+  CANCEL_AGENT_REQUEST: id => id,
+  CANCEL_AGENT_REQUEST_SUCCESS: payload => payload,
+  REJECT_AGENT_REQUEST: id => id,
+  REJECT_AGENT_REQUEST_SUCCESS: payload => payload
 }
