443bb58c5e261e20721834f72f64f9db0d99b981
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 20:00:02 +0700

    add add notification saga

--- client/web/src/state/ducks/companies/notification/sagas.js
+++ client/web/src/state/ducks/companies/notification/sagas.js
@@ -3,7 +3,11 @@ import { takeEvery, put, call, all } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosCompanies'
 import actions from '../../actions'
-import { FETCH_ACCEPT_PENDINGS, FETCH_NOTIFICATIONS } from './types'
+import {
+  FETCH_ACCEPT_PENDINGS,
+  FETCH_NOTIFICATIONS,
+  ADD_NOTIFICATION
+} from './types'
 
 export function* watchFetchAcceptPendings() {
   yield takeEvery(FETCH_ACCEPT_PENDINGS, function*() {
@@ -31,6 +35,18 @@ export function* watchFetchNotifications() {
   })
 }
 
+export function* watchAddNotification() {
+  yield takeEvery(ADD_NOTIFICATION, function*(action) {
+    yield put(
+      actions.company.notification.addNotificationSuccess(action.payload)
+    )
+  })
+}
+
 export default function* rootSaga() {
-  yield all([watchFetchAcceptPendings()])
+  yield all([
+    watchFetchAcceptPendings(),
+    watchFetchNotifications(),
+    watchAddNotification()
+  ])
 }
