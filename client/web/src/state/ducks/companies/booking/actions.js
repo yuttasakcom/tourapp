bb5a70db21f6f25485769edbe4dff2f08117804b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 17:13:07 +0700

    add select booking action

--- client/web/src/state/ducks/companies/booking/actions.js
+++ client/web/src/state/ducks/companies/booking/actions.js
@@ -2,5 +2,6 @@ export default {
   SET_BOOKINGS_STATUS_VISIBILITY_FILTER: state => state,
   FETCH_BOOKINGS: date => date,
   FETCH_BOOKINGS_SUCCESS: payload => payload,
-  UPDATE_BOOKING_STATUS: payload => payload
+  UPDATE_BOOKING_STATUS: payload => payload,
+  SELECT_BOOKING: id => id
 }
