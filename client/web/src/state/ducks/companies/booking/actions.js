9750c68f5123ffc34f769cc40269260f2ed835ce
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 18:01:47 +0700

    add update booking status success action

--- client/web/src/state/ducks/companies/booking/actions.js
+++ client/web/src/state/ducks/companies/booking/actions.js
@@ -3,5 +3,6 @@ export default {
   FETCH_BOOKINGS: date => date,
   FETCH_BOOKINGS_SUCCESS: payload => payload,
   UPDATE_BOOKING_STATUS: payload => payload,
+  UPDATE_BOOKING_STATUS_SUCCESS: payload => payload,
   SELECT_BOOKING: id => id
 }
