d4fde8b646f2a2594f6773fd6c019403760e5de3
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 11:12:15 +0700

    add bookings summary action

--- client/web/src/state/ducks/actions.js
+++ client/web/src/state/ducks/actions.js
@@ -5,13 +5,15 @@ import companyPkg from './companies/pkg/actions'
 import companyBusPath from './companies/busPath/actions'
 import companyPrintBusPath from './companies/printBusPath/actions'
 import companyDashboard from './companies/dashboard/actions'
+import companyBookingsSummary from './companies/bookingsSummary/actions'
 
 export default createActions({
   COMPANY: {
     PKG: companyPkg,
     BUS_PATH: companyBusPath,
     PRINT_BUS_PATH: companyPrintBusPath,
-    DASHBOARD: companyDashboard
+    DASHBOARD: companyDashboard,
+    BOOKINGS_SUMMARY: companyBookingsSummary
   },
   AGENT: {},
   COMMON: {
---
+++ client/web/src/state/ducks/companies/bookingsSummary/actions.js
@@ -0,0 +1,4 @@
+export default {
+  FETCH_BOOKINGS_SUMMARY: null,
+  FETCH_BOOKINGS_SUMMARY_SUCCESS: payload => payload
+}
