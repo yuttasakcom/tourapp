01bcd5d71636ce7225ebfd947f3f7fce79f6f3a8
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-26 15:08:46 +0700

    update reducer

--- client/web/src/state/ducks/companies/booking/reducers.js
+++ client/web/src/state/ducks/companies/booking/reducers.js
@@ -2,27 +2,17 @@ import mapKeys from 'lodash/mapKeys'
 import moment from 'moment'
 import { handleActions } from 'redux-actions'
 
-import {
-  waiting,
-  readed,
-  accepted,
-  rejected,
-  completed
-} from '../../../utils/bookingStatus'
+import { waiting } from '../../../utils/bookingStatus'
 import {
   FETCH_BOOKINGS_SUCCESS,
   SET_BOOKINGS_STATUS_VISIBILITY_FILTER,
-  OPEN_MANAGE_BOOKING_MODAL,
-  CLOSE_MANAGE_BOOKING_MODAL,
-  ACCEPT_BOOKING_SUCCESS,
-  REJECT_BOOKING_SUCCESS,
-  COMPLETE_BOOKING_SUCCESS
+  SELECT_BOOKING,
+  UPDATE_BOOKING_STATUS_SUCCESS
 } from './types'
 
 const initialState = {
   bookings: {},
   selectedBooking: null,
-  showManageBookingModal: false,
   visibilityFilter: { date: moment().startOf('day'), status: waiting }
 }
 
@@ -31,6 +21,11 @@ export default handleActions(
     [SET_BOOKINGS_STATUS_VISIBILITY_FILTER]: (state, action) => ({
       ...state,
       visibilityFilter: { ...state.visibilityFilter, status: action.payload }
+    }),
+
+    [SELECT_BOOKING]: (state, action) => ({
+      ...state,
+      selectedBooking: action.payload
     }),
 
     [FETCH_BOOKINGS_SUCCESS]: (state, action) => ({
@@ -42,68 +37,15 @@ export default handleActions(
       }
     }),
 
-    [OPEN_MANAGE_BOOKING_MODAL]: (state, action) => {
-      if (state.bookings[action.payload].status === waiting) {
-        return {
-          ...state,
-          bookings: {
-            ...state.bookings,
-            [action.payload]: {
-              ...state.bookings[action.payload],
-              status: readed
-            }
-          },
-          showManageBookingModal: true,
-          selectedBooking: action.payload
-        }
-      } else {
-        return {
-          ...state,
-          showManageBookingModal: true,
-          selectedBooking: action.payload
+    [UPDATE_BOOKING_STATUS_SUCCESS]: (state, { payload: { id, status } }) => ({
+      ...state,
+      bookings: {
+        ...state.bookings,
+        [id]: {
+          ...state.bookings[id],
+          status
         }
       }
-    },
-
-    [ACCEPT_BOOKING_SUCCESS]: (state, action) => ({
-      ...state,
-      bookings: {
-        ...state.bookings,
-        [action.payload]: {
-          ...state.bookings[action.payload],
-          status: accepted
-        }
-      },
-      showManageBookingModal: false
-    }),
-
-    [REJECT_BOOKING_SUCCESS]: (state, action) => ({
-      ...state,
-      bookings: {
-        ...state.bookings,
-        [action.payload]: {
-          ...state.bookings[action.payload],
-          status: rejected
-        }
-      },
-      showManageBookingModal: false
-    }),
-
-    [COMPLETE_BOOKING_SUCCESS]: (state, action) => ({
-      ...state,
-      bookings: {
-        ...state.bookings,
-        [action.payload]: {
-          ...state.bookings[action.payload],
-          status: completed
-        }
-      },
-      showManageBookingModal: false
-    }),
-
-    [CLOSE_MANAGE_BOOKING_MODAL]: (state, action) => ({
-      ...state,
-      showManageBookingModal: false
     })
   },
   initialState
