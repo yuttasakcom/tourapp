d67c6124a467296d6038c4f11814acc517d09ab3
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 16:59:14 +0700

    remove add booking success action and type

--- client/web/src/state/ducks/agents/booking/actions.js
+++ client/web/src/state/ducks/agents/booking/actions.js
@@ -3,6 +3,5 @@ export default {
   FETCH_PKGS_SUCCESS: payload => payload,
   FETCH_HOTELS: null,
   FETCH_HOTELS_SUCCESS: payload => payload,
-  ADD_BOOKING: payload => payload,
-  ADD_BOOKING_SUCCESS: payload => payload
+  ADD_BOOKING: payload => payload
 }
