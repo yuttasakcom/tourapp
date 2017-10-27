958a470ace50488e6a41af98bde491fb7d7edb67
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 16:20:53 +0700

    watch agent sagas

--- client/web/src/state/ducks/sagas.js
+++ client/web/src/state/ducks/sagas.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects'
 
 import watchAuth from './auth/sagas'
 import companySagas from './companies/sagas'
+import agentSagas from './agents/sagas'
 
 export default function* rootSaga() {
-  yield all([watchAuth(), companySagas()])
+  yield all([watchAuth(), companySagas(), agentSagas()])
 }
