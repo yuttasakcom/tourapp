76a278214cc7948532324e172ebe1d6504f752ed
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 17:00:08 +0700

    add select pkg action and type

--- client/web/src/state/ducks/agents/booking/actions.js
+++ client/web/src/state/ducks/agents/booking/actions.js
@@ -3,5 +3,6 @@ export default {
   FETCH_PKGS_SUCCESS: payload => payload,
   FETCH_HOTELS: null,
   FETCH_HOTELS_SUCCESS: payload => payload,
-  ADD_BOOKING: payload => payload
+  ADD_BOOKING: payload => payload,
+  SELECT_PKG: id => id
 }
