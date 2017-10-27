536f45712a1b440bbaa4abd3169268b176bf685f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 15:06:39 +0700

    refactor

--- client/web/src/state/ducks/companies/agent/actions.js
+++ client/web/src/state/ducks/companies/agent/actions.js
@@ -1,11 +1,11 @@
 export default {
   FETCH_AGENTS: null,
   FETCH_AGENTS_SUCCESS: payload => payload,
-  FETCH_AGENT_CONTRACT_RATES: id => id,
+  FETCH_AGENT_CONTRACT_RATES: null,
   FETCH_AGENT_CONTRACT_RATES_SUCCESS: payload => payload,
   SELECT_AGENT: id => id,
   SELECT_OFFER_SPECIAL_PRICE_PKG: id => id,
-  OFFER_SPECIAL_PRICE: payload => payload,
+  OFFER_SPECIAL_PRICE: null,
   OFFER_SPECIAL_PRICE_SUCCESS: payload => payload,
   RESET_PRICE: null,
   REQUEST_AGENT: id => id
