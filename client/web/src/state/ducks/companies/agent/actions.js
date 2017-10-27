2e631ec960b2492ca13821e5e46b3e2b7eecf15b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 10:20:01 +0700

    add fetch agent contract rates action and type

--- client/web/src/state/ducks/companies/agent/actions.js
+++ client/web/src/state/ducks/companies/agent/actions.js
@@ -1,5 +1,7 @@
 export default {
   FETCH_AGENTS: null,
   FETCH_AGENTS_SUCCESS: payload => payload,
+  FETCH_AGENT_CONTRACT_RATES: id => id,
+  FETCH_AGENT_CONTRACT_RATES_SUCCESS: payload => payload,
   SELECT_AGENT: id => id
 }
--- client/web/src/state/ducks/companies/agent/types.js
+++ client/web/src/state/ducks/companies/agent/types.js
@@ -1,5 +1,9 @@
 const def = action => `COMPANY/AGENT/${action}`
 export const FETCH_AGENTS = def('FETCH_AGENTS')
 export const FETCH_AGENTS_SUCCESS = def('FETCH_AGENTS_SUCCESS')
+export const FETCH_AGENT_CONTRACT_RATES = def('FETCH_AGENT_CONTRACT_RATES')
+export const FETCH_AGENT_CONTRACT_RATES_SUCCESS = def(
+  'FETCH_AGENT_CONTRACT_RATES_SUCCESS'
+)
 export const REQUEST_AGENT = def('REQUEST_AGENT')
 export const SELECT_AGENT = def('SELECT_AGENT')
