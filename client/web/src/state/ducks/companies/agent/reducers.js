40303d837b2b0fcace6d11f371a0a36c216ae63b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 10:27:38 +0700

    add fetch agent contract rate reducer

--- client/web/src/state/ducks/companies/agent/reducers.js
+++ client/web/src/state/ducks/companies/agent/reducers.js
@@ -1,7 +1,11 @@
 import mapKeys from 'lodash/mapKeys'
 import { handleActions } from 'redux-actions'
 
-import { FETCH_AGENTS_SUCCESS, SELECT_AGENT } from './types'
+import {
+  FETCH_AGENTS_SUCCESS,
+  SELECT_AGENT,
+  FETCH_AGENT_CONTRACT_RATES_SUCCESS
+} from './types'
 
 const initialState = {
   agents: {},
@@ -15,6 +19,11 @@ export default handleActions(
     [FETCH_AGENTS_SUCCESS]: (state, action) => ({
       ...state,
       agents: mapKeys(action.payload, '_id')
+    }),
+
+    [FETCH_AGENT_CONTRACT_RATES_SUCCESS]: (state, action) => ({
+      ...state,
+      selectedAgentContractRates: mapKeys(action.payload, '_id')
     }),
 
     [SELECT_AGENT]: (state, action) => ({
