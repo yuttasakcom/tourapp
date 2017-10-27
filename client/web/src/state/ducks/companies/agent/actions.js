08704f9ccd82f532b8fa28a897538ecdc8a68ef7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-27 13:36:38 +0700

    add reset price action and type

--- client/web/src/state/ducks/companies/agent/actions.js
+++ client/web/src/state/ducks/companies/agent/actions.js
@@ -6,5 +6,6 @@ export default {
   SELECT_AGENT: id => id,
   SELECT_OFFER_SPECIAL_PRICE_PKG: id => id,
   OFFER_SPECIAL_PRICE: payload => payload,
-  OFFER_SPECIAL_PRICE_SUCCESS: payload => payload
+  OFFER_SPECIAL_PRICE_SUCCESS: payload => payload,
+  RESET_PRICE: null
 }
