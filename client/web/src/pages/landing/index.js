5e1dc78eb4319c948fcbcf4bcd1de6f166b64ab7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 16:21:01 +0700

    add auth redirect to landing page

--- client/web/src/pages/landing/index.js
+++ client/web/src/pages/landing/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
@@ -6,6 +8,15 @@ import Card from '../../components/Card'
 
 class Landing extends React.PureComponent {
   render() {
+    const { authenticated, location, role } = this.props
+    const { from } = location.state || {
+      from: {
+        pathname: `/${role === 'company' ? 'companies' : 'agents'}/dashboard`
+      }
+    }
+    if (authenticated) {
+      return <Redirect to={from} />
+    }
     return (
       <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
         <Helmet>
@@ -28,4 +39,9 @@ class Landing extends React.PureComponent {
   }
 }
 
-export default Landing
+const mapStateToProps = ({ auth: { authenticated, user: { role } } }) => ({
+  authenticated,
+  role
+})
+
+export default connect(mapStateToProps)(Landing)
