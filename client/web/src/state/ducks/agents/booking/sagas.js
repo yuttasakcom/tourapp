cc29885cb48d34c946a3fd496b6af6f24fb46312
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 08:43:05 +0700

    add fetch hotel saga

--- client/web/src/state/ducks/agents/booking/sagas.js
+++ client/web/src/state/ducks/agents/booking/sagas.js
@@ -3,7 +3,7 @@ import { takeEvery, put, call, all } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosAgents'
 import actions from '../../actions'
-import { FETCH_PKGS } from './types'
+import { FETCH_PKGS, FETCH_HOTELS } from './types'
 
 export function* watchFetchPkgs() {
   yield takeEvery(FETCH_PKGS, function*() {
@@ -21,6 +21,22 @@ export function* watchFetchPkgs() {
   })
 }
 
+export function* watchFetchHotels() {
+  yield takeEvery(FETCH_HOTELS, function*() {
+    try {
+      const { data } = yield call(axios.get, '/hotels')
+      yield put(actions.agent.booking.fetchHotelsSuccess(data))
+    } catch (e) {
+      yield put(
+        error({
+          title: 'แจ้งเตือน',
+          message: e.response.data
+        })
+      )
+    }
+  })
+}
+
 export default function* rootSaga() {
-  yield all([watchFetchPkgs()])
+  yield all([watchFetchPkgs(), watchFetchHotels()])
 }
