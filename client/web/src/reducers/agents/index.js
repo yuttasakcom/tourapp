1e06207978f41a3e7223915e0ce6418da475d9a2
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 15:09:19 +0700

    add notification reducer

--- client/web/src/reducers/agents/index.js
+++ client/web/src/reducers/agents/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 
-import notification from './notificationReducer'
+import notification from '../../state/ducks/agents/notification/reducers'
 import booking from '../../state/ducks/agents/booking/reducers'
 import manageBooking from '../../state/ducks/agents/manageBooking/reducers'
 import company from '../../state/ducks/agents/company/reducers'
