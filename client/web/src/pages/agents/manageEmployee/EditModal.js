d997e1977f93a49289f3dd4672a62736d306b125
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 10:11:59 +0700

    use internal state and duck

--- client/web/src/pages/agents/manageEmployee/EditModal.js
+++ client/web/src/pages/agents/manageEmployee/EditModal.js
@@ -3,24 +3,25 @@ import { connect } from 'react-redux'
 import Modal from 'react-bootstrap/lib/Modal'
 
 import EmployeeForm from './EmployeeForm'
-import * as actions from '../../../actions/agents'
+import actions from '../../../state/ducks/actions'
 
 class EditModal extends PureComponent {
   onSubmit = values => {
-    const { editEmployee, employee } = this.props
+    const { editEmployee, employee, closeModal } = this.props
     editEmployee(employee, values)
+    closeModal()
   }
 
   render() {
-    const { showModal, closeEditEmployeeModal, employee } = this.props
+    const { showModal, closeModal, employee } = this.props
     return (
-      <Modal show={showModal} onHide={closeEditEmployeeModal}>
+      <Modal show={showModal} onHide={closeModal}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Employee</Modal.Title>
         </Modal.Header>
         <EmployeeForm
           onSubmit={this.onSubmit}
-          closeModal={closeEditEmployeeModal}
+          closeModal={closeModal}
           initialValues={employee}
         />
       </Modal>
@@ -29,8 +30,7 @@ class EditModal extends PureComponent {
 }
 
 const mapStateToProps = ({ agent: { employee } }) => ({
-  showModal: employee.showEditEmployeeModal,
   employee: employee.employees[employee.selectedEmployee]
 })
 
-export default connect(mapStateToProps, actions)(EditModal)
+export default connect(mapStateToProps, actions.agent.employee)(EditModal)
--- client/web/src/pages/agents/manageEmployee/EmployeeDataTable.js
+++ client/web/src/pages/agents/manageEmployee/EmployeeDataTable.js
@@ -5,12 +5,28 @@ import DeleteModal from './DeleteModal'
 import EditModal from './EditModal'
 
 class EmployeeDataTable extends PureComponent {
+  state = {
+    showEditEmployeeModal: false,
+    showDeleteEmployeeModal: false
+  }
+
   render() {
     return (
       <div>
-        <Table />
-        <EditModal />
-        <DeleteModal />
+        <Table
+          openEditEmployeeModal={() =>
+            this.setState({ showEditEmployeeModal: true })}
+          openDeleteEmployeeModal={() =>
+            this.setState({ showDeleteEmployeeModal: true })}
+        />
+        <EditModal
+          showModal={this.state.showEditEmployeeModal}
+          closeModal={() => this.setState({ showEditEmployeeModal: false })}
+        />
+        <DeleteModal
+          showModal={this.state.showDeleteEmployeeModal}
+          closeModal={() => this.setState({ showDeleteEmployeeModal: false })}
+        />
       </div>
     )
   }
