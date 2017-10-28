bfe7ea258edf2fe8e0319cc78ccec41583d4ecea
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 13:25:47 +0700

    use internal state

--- client/web/src/pages/agents/manageCompany/RequestModal.js
+++ client/web/src/pages/agents/manageCompany/RequestModal.js
@@ -12,23 +12,16 @@ class RequestModal extends PureComponent {
   }
 
   render() {
-    const { showModal, closeRequestCompanyModal } = this.props
+    const { showModal, closeModal } = this.props
     return (
-      <Modal show={showModal} onHide={closeRequestCompanyModal}>
+      <Modal show={showModal} onHide={closeModal}>
         <Modal.Header closeButton>
           <Modal.Title>Request Company</Modal.Title>
         </Modal.Header>
-        <RequestForm
-          onSubmit={this.onSubmit}
-          closeModal={closeRequestCompanyModal}
-        />
+        <RequestForm onSubmit={this.onSubmit} closeModal={closeModal} />
       </Modal>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  showModal: state.agent.company.showRequestCompanyModal
-})
-
-export default connect(mapStateToProps, actions)(RequestModal)
+export default connect(null, actions)(RequestModal)
