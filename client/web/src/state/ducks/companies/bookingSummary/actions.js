60b1dba9622a06259c361621563673a830ae7de2
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 11:54:21 +0700

    add date filter

--- client/web/src/state/ducks/companies/bookingSummary/actions.js
+++ client/web/src/state/ducks/companies/bookingSummary/actions.js
@@ -1,4 +1,5 @@
 export default {
   FETCH_BOOKING_SUMMARY: null,
-  FETCH_BOOKING_SUMMARY_SUCCESS: payload => payload
+  FETCH_BOOKING_SUMMARY_SUCCESS: payload => payload,
+  SET_BOOKING_SUMMARY_DATE_VISIBILITY_FILTER: date => date
 }
--- client/web/src/state/ducks/companies/bookingSummary/reducers.js
+++ client/web/src/state/ducks/companies/bookingSummary/reducers.js
@@ -1,7 +1,10 @@
 import moment from 'moment'
 import { handleActions } from 'redux-actions'
 
-import { FETCH_BOOKING_SUMMARY_SUCCESS } from './types'
+import {
+  FETCH_BOOKING_SUMMARY_SUCCESS,
+  SET_BOOKING_SUMMARY_DATE_VISIBILITY_FILTER
+} from './types'
 
 const initialState = {
   bookingSummary: [],
@@ -13,6 +16,11 @@ export default handleActions(
     [FETCH_BOOKING_SUMMARY_SUCCESS]: (state, action) => ({
       ...state,
       bookingSummary: action.payload
+    }),
+
+    [SET_BOOKING_SUMMARY_DATE_VISIBILITY_FILTER]: (state, action) => ({
+      ...state,
+      visibilityFilter: { ...state.visibilityFilter, date: action.payload }
     })
   },
   initialState
