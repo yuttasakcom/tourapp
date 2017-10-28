9cb959a95c752a041517a09d37f1d7414575365f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 08:59:05 +0700

    add add booking saga

--- client/web/src/state/ducks/agents/booking/sagas.js
+++ client/web/src/state/ducks/agents/booking/sagas.js
@@ -1,9 +1,11 @@
-import { error } from 'react-notification-system-redux'
+import { error, success } from 'react-notification-system-redux'
 import { takeEvery, put, call, all } from 'redux-saga/effects'
 
 import axios from '../../../utils/axiosAgents'
 import actions from '../../actions'
-import { FETCH_PKGS, FETCH_HOTELS } from './types'
+import socket from '../../../utils/socket'
+import { openAgentReport } from '../../../utils'
+import { FETCH_PKGS, FETCH_HOTELS, ADD_BOOKING } from './types'
 
 export function* watchFetchPkgs() {
   yield takeEvery(FETCH_PKGS, function*() {
@@ -37,6 +39,30 @@ export function* watchFetchHotels() {
   })
 }
 
+export function* watchAddBooking() {
+  yield takeEvery(ADD_BOOKING, function*(action) {
+    try {
+      const { data } = yield call(axios.post, '/bookings', action.payload)
+      yield call(openAgentReport, `voucher?bookingId=${data._id}`)
+      yield put(actions.agent.booking.addBookingSuccess(data))
+      yield put(
+        success({
+          title: 'แจ้งเตือน',
+          message: 'จองแพ็คเกจสำเร็จแล้ว!'
+        })
+      )
+      socket.emit('book', data)
+    } catch (e) {
+      yield put(
+        error({
+          title: 'แจ้งเตือน',
+          message: e.response.data
+        })
+      )
+    }
+  })
+}
+
 export default function* rootSaga() {
-  yield all([watchFetchPkgs(), watchFetchHotels()])
+  yield all([watchFetchPkgs(), watchFetchHotels(), watchAddBooking()])
 }
