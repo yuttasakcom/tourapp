07627ab4c4e520a42c19b3eabb40875a43b64d5e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 09:48:56 +0700

    add select employee action and type

--- client/web/src/state/ducks/agents/employee/actions.js
+++ client/web/src/state/ducks/agents/employee/actions.js
@@ -3,5 +3,6 @@ export default {
   FETCH_EMPLOYEES_SUCCESS: payload => payload,
   ADD_EMPLOYEE: payload => payload,
   EDIT_EMPLOYEE: payload => payload,
-  DELETE_EMPLOYEE: id => id
+  DELETE_EMPLOYEE: id => id,
+  SELECT_EMPLOYEE: id => id
 }
