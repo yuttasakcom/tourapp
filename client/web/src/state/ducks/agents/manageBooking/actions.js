4edaed61b688d669232f5305ef620fdbec2cb1c0
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 10:52:25 +0700

    fix typo

--- client/web/src/state/ducks/agents/manageBooking/actions.js
+++ client/web/src/state/ducks/agents/manageBooking/actions.js
@@ -1,6 +1,6 @@
 export default {
   FETCH_BOOKINGS: null,
   FETCH_BOOKINGS_SUCCESS: payload => payload,
-  SET_BOOKINGS_STATUS_VISIBILITY_FILTER: state => state,
+  SET_BOOKINGS_STATUS_VISIBILITY_FILTER: status => status,
   SELECT_BOOKING: id => id
 }
