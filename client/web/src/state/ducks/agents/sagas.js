09ed2f884b5c85f93dedabf8d3029d3f5d650812
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-10-28 10:44:41 +0700

    add fetch bookings saga

--- client/web/src/state/ducks/agents/sagas.js
+++ client/web/src/state/ducks/agents/sagas.js
@@ -3,7 +3,13 @@ import { all } from 'redux-saga/effects'
 import rootDashboardSaga from './dashboard/sagas'
 import rootBookingSaga from './booking/sagas'
 import rootEmployeeSaga from './employee/sagas'
+import rootManageBookingSaga from './manageBooking/sagas'
 
 export default function* rootSaga() {
-  yield all([rootDashboardSaga(), rootBookingSaga(), rootEmployeeSaga()])
+  yield all([
+    rootDashboardSaga(),
+    rootBookingSaga(),
+    rootEmployeeSaga(),
+    rootManageBookingSaga()
+  ])
 }
